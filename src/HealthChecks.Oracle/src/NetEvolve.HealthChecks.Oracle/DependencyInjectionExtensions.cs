022a456a66e8b32dcaa00996414c7e68149d9253
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-09-10 22:25:43 +0200

    feat: Added Oracle Database checks

---
+++ src/HealthChecks.Oracle/src/NetEvolve.HealthChecks.Oracle/DependencyInjectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿namespace NetEvolve.HealthChecks.Oracle;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetEvolve.Arguments;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+/// <summary>
+/// Extensions methods for <see cref="IHealthChecksBuilder"/> with custom Health Checks.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+    /// <summary>
+    /// Add a health check for the MySql database.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="name">The name of the <see cref="OracleCheck"/>.</param>
+    /// <param name="options">An optional action to configure.</param>
+    /// <param name="tags">A list of additional tags that can be used to filter sets of health checks. Optional.</param>
+    /// <exception cref="ArgumentNullException">The <paramref name="builder"/> is <see langword="null" />.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="name"/> is <see langword="null" />.</exception>
+    /// <exception cref="ArgumentException">The <paramref name="name"/> is <see langword="null" /> or <c>whitespace</c>.</exception>
+    /// <exception cref="ArgumentException">The <paramref name="name"/> is already in use.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="tags"/> is <see langword="null" />.</exception>
+    public static IHealthChecksBuilder AddOracle(
+        [NotNull] this IHealthChecksBuilder builder,
+        [NotNull] string name,
+        Action<OracleOptions>? options = null,
+        params string[] tags
+    )
+    {
+        Argument.ThrowIfNull(builder);
+        Argument.ThrowIfNullOrEmpty(name);
+        Argument.ThrowIfNull(tags);
+
+        if (!builder.IsServiceTypeRegistered<OracleCheckMarker>())
+        {
+            _ = builder.Services
+                .AddSingleton<OracleCheckMarker>()
+                .AddSingleton<OracleCheck>()
+                .ConfigureOptions<OracleConfigure>();
+        }
+
+        var internalName = name.EnsureStartsWith("Oracle", StringComparison.OrdinalIgnoreCase);
+
+        if (builder.IsNameAlreadyUsed(internalName))
+        {
+            throw new ArgumentException($"Name `{name}` already in use.", nameof(name), null);
+        }
+
+        if (options is not null)
+        {
+            _ = builder.Services.Configure(internalName, options);
+        }
+
+        return builder.AddCheck<OracleCheck>(
+            internalName,
+            HealthStatus.Unhealthy,
+            new[] { "oracle", "database" }.Union(tags, StringComparer.OrdinalIgnoreCase)
+        );
+    }
+
+    private sealed partial class OracleCheckMarker { }
+}
---
+++ src/HealthChecks.Oracle/tests/NetEvolve.HealthChecks.Oracle.Tests.Integration/OracleCheckTests.cs
@@ -0,0 +1,164 @@
+﻿namespace NetEvolve.HealthChecks.Oracle.Tests.Integration;
+
+using Microsoft.Extensions.Configuration;
+using NetEvolve.Extensions.XUnit;
+using NetEvolve.HealthChecks.Tests;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Xunit;
+
+[IntegrationTest]
+[ExcludeFromCodeCoverage]
+public class OracleCheckTests : HealthCheckTestBase, IClassFixture<OracleDatabase>
+{
+    private readonly OracleDatabase _database;
+
+    public OracleCheckTests(OracleDatabase database) => _database = database;
+
+    [Fact]
+    public async Task AddOracle_UseOptions_ShouldReturnHealthy() =>
+        await RunAndVerify(healthChecks =>
+            {
+                _ = healthChecks.AddOracle(
+                    "TestContainerHealthy",
+                    options =>
+                    {
+                        options.ConnectionString = _database.GetConnectionString();
+                    }
+                );
+            })
+            .ConfigureAwait(false);
+
+    [Fact]
+    public async Task AddOracle_UseOptionsDoubleRegistered_ShouldReturnHealthy() =>
+        _ = await Assert
+            .ThrowsAsync<ArgumentException>(
+                "name",
+                async () =>
+                {
+                    await RunAndVerify(healthChecks =>
+                        {
+                            _ = healthChecks
+                                .AddOracle("TestContainerHealthy")
+                                .AddOracle("TestContainerHealthy");
+                        })
+                        .ConfigureAwait(false);
+                }
+            )
+            .ConfigureAwait(false);
+
+    [Fact]
+    public async Task AddOracle_UseOptions_ShouldReturnDegraded() =>
+        await RunAndVerify(healthChecks =>
+            {
+                _ = healthChecks.AddOracle(
+                    "TestContainerDegraded",
+                    options =>
+                    {
+                        options.ConnectionString = _database.GetConnectionString();
+                        options.Timeout = 0;
+                    }
+                );
+            })
+            .ConfigureAwait(false);
+
+    [Fact]
+    public async Task AddOracle_UseOptions_ShouldReturnUnhealthy() =>
+        await RunAndVerify(healthChecks =>
+            {
+                _ = healthChecks.AddOracle(
+                    "TestContainerUnhealthy",
+                    options =>
+                    {
+                        options.ConnectionString = _database.GetConnectionString();
+                        options.Command = "SELECT 1 = `1`;";
+                    }
+                );
+            })
+            .ConfigureAwait(false);
+
+    [Fact]
+    public async Task AddOracle_UseConfiguration_ShouldReturnHealthy() =>
+        await RunAndVerify(
+                healthChecks =>
+                {
+                    _ = healthChecks.AddOracle("TestContainerHealthy");
+                },
+                config =>
+                {
+                    var values = new Dictionary<string, string?>
+                    {
+                        {
+                            "HealthChecks:OracleTestContainerHealthy:ConnectionString",
+                            _database.GetConnectionString()
+                        }
+                    };
+                    _ = config.AddInMemoryCollection(values);
+                }
+            )
+            .ConfigureAwait(false);
+
+    [Fact]
+    public async Task AddOracle_UseConfiguration_ShouldReturnDegraded() =>
+        await RunAndVerify(
+                healthChecks =>
+                {
+                    _ = healthChecks.AddOracle("TestContainerDegraded");
+                },
+                config =>
+                {
+                    var values = new Dictionary<string, string?>
+                    {
+                        {
+                            "HealthChecks:OracleTestContainerDegraded:ConnectionString",
+                            _database.GetConnectionString()
+                        },
+                        { "HealthChecks:OracleTestContainerDegraded:Timeout", "0" }
+                    };
+                    _ = config.AddInMemoryCollection(values);
+                }
+            )
+            .ConfigureAwait(false);
+
+    [Fact]
+    public async Task AddOracle_UseConfigration_ConnectionStringEmpty_ThrowException() =>
+        await RunAndVerify(
+                healthChecks =>
+                {
+                    _ = healthChecks.AddOracle("TestNoValues");
+                },
+                config =>
+                {
+                    var values = new Dictionary<string, string?>
+                    {
+                        { "HealthChecks:OracleTestNoValues:ConnectionString", "" }
+                    };
+                    _ = config.AddInMemoryCollection(values);
+                }
+            )
+            .ConfigureAwait(false);
+
+    [Fact]
+    public async Task AddOracle_UseConfigration_TimeoutMinusTwo_ThrowException() =>
+        await RunAndVerify(
+                healthChecks =>
+                {
+                    _ = healthChecks.AddOracle("TestNoValues");
+                },
+                config =>
+                {
+                    var values = new Dictionary<string, string?>
+                    {
+                        {
+                            "HealthChecks:OracleTestNoValues:ConnectionString",
+                            _database.GetConnectionString()
+                        },
+                        { "HealthChecks:OracleTestNoValues:Timeout", "-2" }
+                    };
+                    _ = config.AddInMemoryCollection(values);
+                }
+            )
+            .ConfigureAwait(false);
+}
