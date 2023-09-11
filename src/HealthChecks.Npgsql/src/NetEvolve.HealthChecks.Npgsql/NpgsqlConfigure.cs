795249f5e3d117b79db4a7b6c7c1230afcf23a30
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-09-11 20:55:09 +0200

    feat: Added `PostgreSql` checks

---
+++ src/HealthChecks.Npgsql/src/NetEvolve.HealthChecks.Npgsql/NpgsqlConfigure.cs
@@ -0,0 +1,64 @@
+﻿namespace NetEvolve.HealthChecks.Npgsql;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using NetEvolve.Arguments;
+using System.Threading;
+
+using static Microsoft.Extensions.Options.ValidateOptionsResult;
+
+internal sealed class NpgsqlConfigure
+    : IConfigureNamedOptions<NpgsqlOptions>,
+        IPostConfigureOptions<NpgsqlOptions>,
+        IValidateOptions<NpgsqlOptions>
+{
+    private readonly IConfiguration _configuration;
+
+    public NpgsqlConfigure(IConfiguration configuration) => _configuration = configuration;
+
+    public void Configure(string? name, NpgsqlOptions options)
+    {
+        Argument.ThrowIfNullOrWhiteSpace(name);
+        _configuration.Bind($"HealthChecks:{name}", options);
+    }
+
+    public void Configure(NpgsqlOptions options) => Configure(Options.DefaultName, options);
+
+    public void PostConfigure(string? name, NpgsqlOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(options.Command))
+        {
+            options.Command = NpgsqlCheck.DefaultCommand;
+        }
+    }
+
+    public ValidateOptionsResult Validate(string? name, NpgsqlOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return Fail("The name cannot be null or whitespace.");
+        }
+
+        if (options is null)
+        {
+            return Fail("The option cannot be null.");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.ConnectionString))
+        {
+            return Fail("The connection string cannot be null or whitespace.");
+        }
+
+        if (options.Timeout < Timeout.Infinite)
+        {
+            return Fail("The timeout cannot be less than infinite (-1).");
+        }
+
+        return Success;
+    }
+}
---
+++ src/HealthChecks.Npgsql/tests/NetEvolve.HealthChecks.Npgsql.Tests.Integration/NpgsqlDatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace NetEvolve.HealthChecks.Npgsql.Tests.Integration;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+public sealed class NpgsqlDatabase : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _database = new PostgreSqlBuilder().Build();
+
+    public string ConnectionString => _database.GetConnectionString();
+
+    public async Task DisposeAsync() => await _database.DisposeAsync().ConfigureAwait(false);
+
+    public async Task InitializeAsync() => await _database.StartAsync().ConfigureAwait(false);
+}
