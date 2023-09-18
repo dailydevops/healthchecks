6eb0c1ab6a456d87a2ef8d264c07e58f8bf14f5b
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-09-18 19:44:28 +0200

    fix: Removed `.ConfigureAwait(false)` to fix `xUnit1030`

--- src/HealthChecks.SQLite/tests/NetEvolve.HealthChecks.SQLite.Tests.Unit/SQLiteCheckTests.cs
+++ src/HealthChecks.SQLite/tests/NetEvolve.HealthChecks.SQLite.Tests.Unit/SQLiteCheckTests.cs
@@ -22,10 +22,10 @@ public sealed class SQLiteCheckTests
         var check = new SQLiteCheck(optionsMonitor);
 
         // Act
-        async Task Act() => _ = await check.CheckHealthAsync(null!).ConfigureAwait(false);
+        async Task Act() => _ = await check.CheckHealthAsync(null!);
 
         // Assert
-        _ = await Assert.ThrowsAsync<ArgumentNullException>("context", Act).ConfigureAwait(false);
+        _ = await Assert.ThrowsAsync<ArgumentNullException>("context", Act);
     }
 
     [Fact]
@@ -41,7 +41,7 @@ public sealed class SQLiteCheckTests
         var cancellationToken = new CancellationToken(true);
 
         // Act
-        var result = await check.CheckHealthAsync(context, cancellationToken).ConfigureAwait(false);
+        var result = await check.CheckHealthAsync(context, cancellationToken);
 
         // Assert
         Assert.Equal(HealthStatus.Unhealthy, result.Status);
@@ -60,7 +60,7 @@ public sealed class SQLiteCheckTests
         };
 
         // Act
-        var result = await check.CheckHealthAsync(context).ConfigureAwait(false);
+        var result = await check.CheckHealthAsync(context);
 
         // Assert
         Assert.Equal(HealthStatus.Unhealthy, result.Status);
--- src/HealthChecks.SqlServer.Legacy/tests/NetEvolve.HealthChecks.SqlServer.Legacy.Tests.Integration/SqlServerLegacyCheckTests.cs
+++ src/HealthChecks.SqlServer.Legacy/tests/NetEvolve.HealthChecks.SqlServer.Legacy.Tests.Integration/SqlServerLegacyCheckTests.cs
@@ -21,146 +21,136 @@ public class SqlServerLegacyCheckTests : HealthCheckTestBase, IClassFixture<SqlS
     [Fact]
     public async Task AddSqlServerLegacy_UseOptions_ShouldReturnHealthy() =>
         await RunAndVerify(healthChecks =>
-            {
-                _ = healthChecks.AddSqlServerLegacy(
-                    "TestContainerHealthy",
-                    options =>
-                    {
-                        options.ConnectionString = _database.ConnectionString;
-                    }
-                );
-            })
-            .ConfigureAwait(false);
+        {
+            _ = healthChecks.AddSqlServerLegacy(
+                "TestContainerHealthy",
+                options =>
+                {
+                    options.ConnectionString = _database.ConnectionString;
+                }
+            );
+        });
 
     [Fact]
     public async Task AddSqlServerLegacy_UseOptionsDoubleRegistered_ShouldReturnHealthy() =>
-        _ = await Assert
-            .ThrowsAsync<ArgumentException>(
-                "name",
-                async () =>
+        _ = await Assert.ThrowsAsync<ArgumentException>(
+            "name",
+            async () =>
+            {
+                await RunAndVerify(healthChecks =>
                 {
-                    await RunAndVerify(healthChecks =>
-                        {
-                            _ = healthChecks
-                                .AddSqlServerLegacy("TestContainerHealthy")
-                                .AddSqlServerLegacy("TestContainerHealthy");
-                        })
-                        .ConfigureAwait(false);
-                }
-            )
-            .ConfigureAwait(false);
+                    _ = healthChecks
+                        .AddSqlServerLegacy("TestContainerHealthy")
+                        .AddSqlServerLegacy("TestContainerHealthy");
+                });
+            }
+        );
 
     [Fact]
     public async Task AddSqlServerLegacy_UseOptions_ShouldReturnDegraded() =>
         await RunAndVerify(healthChecks =>
-            {
-                _ = healthChecks.AddSqlServerLegacy(
-                    "TestContainerDegraded",
-                    options =>
-                    {
-                        options.ConnectionString = _database.ConnectionString;
-                        options.Command = "SELECT 1; WAITFOR DELAY '00:00:00.100';";
-                        options.Timeout = 0;
-                    }
-                );
-            })
-            .ConfigureAwait(false);
+        {
+            _ = healthChecks.AddSqlServerLegacy(
+                "TestContainerDegraded",
+                options =>
+                {
+                    options.ConnectionString = _database.ConnectionString;
+                    options.Command = "SELECT 1; WAITFOR DELAY '00:00:00.100';";
+                    options.Timeout = 0;
+                }
+            );
+        });
 
     [Fact]
     public async Task AddSqlServerLegacy_UseOptions_ShouldReturnUnhealthy() =>
         await RunAndVerify(healthChecks =>
-            {
-                _ = healthChecks.AddSqlServerLegacy(
-                    "TestContainerUnhealthy",
-                    options =>
-                    {
-                        options.ConnectionString = _database.ConnectionString;
-                        options.Command = "RAISERROR('This is a test.',16,1)";
-                    }
-                );
-            })
-            .ConfigureAwait(false);
+        {
+            _ = healthChecks.AddSqlServerLegacy(
+                "TestContainerUnhealthy",
+                options =>
+                {
+                    options.ConnectionString = _database.ConnectionString;
+                    options.Command = "RAISERROR('This is a test.',16,1)";
+                }
+            );
+        });
 
     [Fact]
     public async Task AddSqlServerLegacy_UseConfiguration_ShouldReturnHealthy() =>
         await RunAndVerify(
-                healthChecks =>
+            healthChecks =>
+            {
+                _ = healthChecks.AddSqlServerLegacy("TestContainerHealthy");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
                 {
-                    _ = healthChecks.AddSqlServerLegacy("TestContainerHealthy");
-                },
-                config =>
-                {
-                    var values = new Dictionary<string, string?>
                     {
-                        {
-                            "HealthChecks:SqlServerTestContainerHealthy:ConnectionString",
-                            _database.ConnectionString
-                        }
-                    };
-                    _ = config.AddInMemoryCollection(values);
-                }
-            )
-            .ConfigureAwait(false);
+                        "HealthChecks:SqlServerTestContainerHealthy:ConnectionString",
+                        _database.ConnectionString
+                    }
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
 
     [Fact]
     public async Task AddSqlServerLegacy_UseConfiguration_ShouldReturnDegraded() =>
         await RunAndVerify(
-                healthChecks =>
+            healthChecks =>
+            {
+                _ = healthChecks.AddSqlServerLegacy("TestContainerDegraded");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
                 {
-                    _ = healthChecks.AddSqlServerLegacy("TestContainerDegraded");
-                },
-                config =>
-                {
-                    var values = new Dictionary<string, string?>
                     {
-                        {
-                            "HealthChecks:SqlServerTestContainerDegraded:ConnectionString",
-                            _database.ConnectionString
-                        },
-                        { "HealthChecks:SqlServerTestContainerDegraded:Timeout", "0" }
-                    };
-                    _ = config.AddInMemoryCollection(values);
-                }
-            )
-            .ConfigureAwait(false);
+                        "HealthChecks:SqlServerTestContainerDegraded:ConnectionString",
+                        _database.ConnectionString
+                    },
+                    { "HealthChecks:SqlServerTestContainerDegraded:Timeout", "0" }
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
 
     [Fact]
     public async Task AddSqlServerLegacy_UseConfigration_ConnectionStringEmpty_ThrowException() =>
         await RunAndVerify(
-                healthChecks =>
+            healthChecks =>
+            {
+                _ = healthChecks.AddSqlServerLegacy("TestNoValues");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
                 {
-                    _ = healthChecks.AddSqlServerLegacy("TestNoValues");
-                },
-                config =>
-                {
-                    var values = new Dictionary<string, string?>
-                    {
-                        { "HealthChecks:SqlServerTestNoValues:ConnectionString", "" }
-                    };
-                    _ = config.AddInMemoryCollection(values);
-                }
-            )
-            .ConfigureAwait(false);
+                    { "HealthChecks:SqlServerTestNoValues:ConnectionString", "" }
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
 
     [Fact]
     public async Task AddSqlServerLegacy_UseConfigration_TimeoutMinusTwo_ThrowException() =>
         await RunAndVerify(
-                healthChecks =>
+            healthChecks =>
+            {
+                _ = healthChecks.AddSqlServerLegacy("TestNoValues");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
                 {
-                    _ = healthChecks.AddSqlServerLegacy("TestNoValues");
-                },
-                config =>
-                {
-                    var values = new Dictionary<string, string?>
                     {
-                        {
-                            "HealthChecks:SqlServerTestNoValues:ConnectionString",
-                            _database.ConnectionString
-                        },
-                        { "HealthChecks:SqlServerTestNoValues:Timeout", "-2" }
-                    };
-                    _ = config.AddInMemoryCollection(values);
-                }
-            )
-            .ConfigureAwait(false);
+                        "HealthChecks:SqlServerTestNoValues:ConnectionString",
+                        _database.ConnectionString
+                    },
+                    { "HealthChecks:SqlServerTestNoValues:Timeout", "-2" }
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
 }
