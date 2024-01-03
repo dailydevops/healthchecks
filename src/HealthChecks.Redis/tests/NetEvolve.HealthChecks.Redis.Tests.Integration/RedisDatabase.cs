6dcb2507fae54babbf52217dfd1fe808869f589e
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2024-01-03 20:57:20 +0100

    feat: Redis Implementation

---
+++ src/HealthChecks.Redis/tests/NetEvolve.HealthChecks.Redis.Tests.Integration/RedisDatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace NetEvolve.HealthChecks.Redis.Tests.Integration;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Testcontainers.Redis;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+public sealed class RedisDatabase : IAsyncLifetime
+{
+    private readonly RedisContainer _database = new RedisBuilder().Build();
+
+    public string GetConnectionString() => _database.GetConnectionString();
+
+    public async Task DisposeAsync() => await _database.DisposeAsync().ConfigureAwait(false);
+
+    public async Task InitializeAsync() => await _database.StartAsync().ConfigureAwait(false);
+}
---
+++ src/HealthChecks.Redis/tests/NetEvolve.HealthChecks.Redis.Tests.Unit/DependencyInjectionExtensionsTests.cs
@@ -0,0 +1,90 @@
+﻿namespace NetEvolve.HealthChecks.Redis.Tests.Unit;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetEvolve.Extensions.XUnit;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+[UnitTest]
+public class DependencyInjectionExtensionsTests
+{
+    [Fact]
+    public void AddSQLite_WhenArgumentBuilderNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var builder = default(IHealthChecksBuilder);
+
+        // Act
+        void Act() => _ = builder.AddRedisDatabase("Test");
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("builder", Act);
+    }
+
+    [Fact]
+    public void AddSQLite_WhenArgumentNameNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var name = default(string);
+
+        // Act
+        void Act() => _ = builder.AddRedisDatabase(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("name", Act);
+    }
+
+    [Fact]
+    public void AddSQLite_WhenArgumentNameEmpty_ThrowArgumentException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var name = string.Empty;
+
+        // Act
+        void Act() => _ = builder.AddRedisDatabase(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>("name", Act);
+    }
+
+    [Fact]
+    public void AddSQLite_WhenArgumentTagsNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var tags = default(string[]);
+
+        // Act
+        void Act() => _ = builder.AddRedisDatabase("Test", tags: tags);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("tags", Act);
+    }
+
+    [Fact]
+    public void AddSQLite_WhenArgumentNameIsAlreadyUsed_ThrowArgumentException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        const string name = "Test";
+
+        // Act
+        void Act() => _ = builder.AddRedisDatabase(name).AddRedisDatabase(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>(nameof(name), Act);
+    }
+}
