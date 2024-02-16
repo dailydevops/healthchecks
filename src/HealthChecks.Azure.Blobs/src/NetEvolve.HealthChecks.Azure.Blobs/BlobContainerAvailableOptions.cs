9bae66e60f3234fdd1d5e4a60476e7e8bc9e2c4b
Martin Stühmer <me@samtrion.net>
2024-02-16 13:06:23 +0100

    feat: Added `Azure.Blobs`

---
+++ src/HealthChecks.Azure.Blobs/src/NetEvolve.HealthChecks.Azure.Blobs/BlobContainerAvailableOptions.cs
@@ -0,0 +1,26 @@
+﻿namespace NetEvolve.HealthChecks.Azure.Blobs;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using global::Azure.Storage.Blobs;
+
+/// <summary>
+/// Options for the <see cref="BlobContainerAvailableHealthCheck"/>.
+/// </summary>
+public sealed class BlobContainerAvailableOptions
+{
+    public string? ConnectionString { get; set; }
+    public ClientCreationMode Mode { get; set; }
+    public int Timeout { get; set; } = 100;
+
+    public Uri? ServiceUri { get; set; }
+    public string? AccountName { get; set; }
+    public string? AccountKey { get; set; }
+
+    [SuppressMessage(
+        "Design",
+        "CA1044:Properties should not be write only",
+        Justification = "As designed."
+    )]
+    public Action<BlobClientOptions>? ConfigureClientOptions { internal get; set; }
+}
---
+++ src/HealthChecks.Azure.Blobs/src/NetEvolve.HealthChecks.Azure.Blobs/DependencyInjectionExtensions.cs
@@ -0,0 +1,63 @@
+﻿namespace NetEvolve.HealthChecks.Azure.Blobs;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetEvolve.Arguments;
+
+/// <summary>
+/// Extensions methods for <see cref="IHealthChecksBuilder"/> with custom Health Checks.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+    /// <summary>
+    /// Adds a health check for the Azure Blob Storage, to check the availability of this service.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>. This is prefixed internally with `AzureBlobAvailability` if not already present.</param>
+    /// <param name="name">The name of the <see cref="BlobContainerAvailableHealthCheck"/>.</param>
+    /// <param name="options">An optional action to configure.</param>
+    /// <param name="tags">A list of additional tags that can be used to filter sets of health checks. Optional.</param>
+    /// <exception cref="ArgumentNullException">The <paramref name="builder"/> is <see langword="null" />.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="name"/> is <see langword="null" />.</exception>
+    /// <exception cref="ArgumentException">The <paramref name="name"/> is <see langword="null" /> or <c>whitespace</c>.</exception>
+    /// <exception cref="ArgumentException">The <paramref name="name"/> is already in use.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="tags"/> is <see langword="null" />.</exception>
+    public static IHealthChecksBuilder AddAzureBlobAvailability(
+        [NotNull] this IHealthChecksBuilder builder,
+        [NotNull] string name,
+        Action<BlobContainerAvailableOptions>? options = null,
+        params string[] tags
+    )
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        Argument.ThrowIfNullOrEmpty(name);
+        ArgumentNullException.ThrowIfNull(tags);
+
+        if (!builder.IsServiceTypeRegistered<AzureBlobCheckMarker>())
+        {
+            _ = builder
+                .Services.AddSingleton<AzureBlobCheckMarker>()
+                .AddSingleton<BlobContainerAvailableHealthCheck>()
+                .ConfigureOptions<BlobContainerAvailableConfigure>();
+        }
+
+        if (builder.IsNameAlreadyUsed<BlobContainerAvailableHealthCheck>(name))
+        {
+            throw new ArgumentException($"Name `{name}` already in use.", nameof(name), null);
+        }
+
+        if (options is not null)
+        {
+            _ = builder.Services.Configure(name, options);
+        }
+
+        return builder.AddCheck<BlobContainerAvailableHealthCheck>(
+            name,
+            HealthStatus.Unhealthy,
+            tags
+        );
+    }
+
+    private sealed partial class AzureBlobCheckMarker { }
+}
