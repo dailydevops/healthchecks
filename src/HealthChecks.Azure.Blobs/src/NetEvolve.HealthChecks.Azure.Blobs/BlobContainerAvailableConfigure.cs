1a6ed480076f781f93e297062d1801618cce9328
Martin Stühmer <me@samtrion.net>
2024-02-16 13:06:23 +0100

    feat: Added `Azure.Blobs`

---
+++ src/HealthChecks.Azure.Blobs/src/NetEvolve.HealthChecks.Azure.Blobs/BlobContainerAvailableConfigure.cs
@@ -0,0 +1,186 @@
+﻿namespace NetEvolve.HealthChecks.Azure.Blobs;
+
+using System;
+using System.Threading;
+using global::Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NetEvolve.Arguments;
+using static Microsoft.Extensions.Options.ValidateOptionsResult;
+
+internal sealed class BlobContainerAvailableConfigure
+    : IConfigureNamedOptions<BlobContainerAvailableOptions>,
+        IPostConfigureOptions<BlobContainerAvailableOptions>,
+        IValidateOptions<BlobContainerAvailableOptions>
+{
+    private readonly IConfiguration _configuration;
+    private readonly IServiceProvider _serviceProvider;
+
+    public BlobContainerAvailableConfigure(
+        IConfiguration configuration,
+        IServiceProvider serviceProvider
+    )
+    {
+        _configuration = configuration;
+        _serviceProvider = serviceProvider;
+    }
+
+    public void Configure(string? name, BlobContainerAvailableOptions options)
+    {
+        Argument.ThrowIfNullOrWhiteSpace(name);
+        _configuration.Bind($"HealthChecks:AzureBlobAvailability:{name}", options);
+    }
+
+    public void Configure(BlobContainerAvailableOptions options) =>
+        Configure(Options.DefaultName, options);
+
+    public void PostConfigure(string? name, BlobContainerAvailableOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(name) || options.Mode == ClientCreationMode.ServiceProvider)
+        {
+            return;
+        }
+    }
+
+    public ValidateOptionsResult Validate(string? name, BlobContainerAvailableOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return Fail("The name cannot be null or whitespace.");
+        }
+
+        if (options is null)
+        {
+            return Fail("The option cannot be null.");
+        }
+
+        if (options.Timeout < Timeout.Infinite)
+        {
+            return Fail("The timeout cannot be less than infinite (-1).");
+        }
+
+        var mode = options.Mode;
+
+        return options.Mode switch
+        {
+            ClientCreationMode.ServiceProvider => ValidateModeServiceProvider(),
+            ClientCreationMode.ConnectionString => ValidateModeConnectionString(options),
+            ClientCreationMode.DefaultAzureCredentials
+                => ValidateModeDefaultAzureCredentials(options),
+            ClientCreationMode.SharedKey => ValidateModeSharedKey(options),
+            ClientCreationMode.AzureSasCredential => ValidateModeAzureSasCredential(options),
+            _ => Fail($"The mode `{mode}` is not supported."),
+        };
+    }
+
+    private static ValidateOptionsResult ValidateModeAzureSasCredential(
+        BlobContainerAvailableOptions options
+    )
+    {
+        if (options.ServiceUri is null)
+        {
+            return Fail(
+                $"The service url cannot be null when using `{nameof(ClientCreationMode.AzureSasCredential)}` mode."
+            );
+        }
+
+        if (!options.ServiceUri.IsAbsoluteUri)
+        {
+            return Fail(
+                $"The service url must be an absolute url when using `{nameof(ClientCreationMode.AzureSasCredential)}` mode."
+            );
+        }
+
+        if (string.IsNullOrWhiteSpace(options.ServiceUri.Query))
+        {
+            return Fail(
+                $"The sas query token cannot be null or whitespace when using `{nameof(ClientCreationMode.AzureSasCredential)}` mode."
+            );
+        }
+
+        return Success;
+    }
+
+    private static ValidateOptionsResult ValidateModeSharedKey(
+        BlobContainerAvailableOptions options
+    )
+    {
+        if (options.ServiceUri is null)
+        {
+            return Fail(
+                $"The service url cannot be null when using `{nameof(ClientCreationMode.SharedKey)}` mode."
+            );
+        }
+
+        if (!options.ServiceUri.IsAbsoluteUri)
+        {
+            return Fail(
+                $"The service url must be an absolute url when using `{nameof(ClientCreationMode.SharedKey)}` mode."
+            );
+        }
+
+        if (string.IsNullOrWhiteSpace(options.AccountName))
+        {
+            return Fail(
+                $"The account name cannot be null or whitespace when using `{nameof(ClientCreationMode.SharedKey)}` mode."
+            );
+        }
+
+        if (string.IsNullOrWhiteSpace(options.AccountKey))
+        {
+            return Fail(
+                $"The account key cannot be null or whitespace when using `{nameof(ClientCreationMode.SharedKey)}` mode."
+            );
+        }
+
+        return Success;
+    }
+
+    private static ValidateOptionsResult ValidateModeDefaultAzureCredentials(
+        BlobContainerAvailableOptions options
+    )
+    {
+        if (options.ServiceUri is null)
+        {
+            return Fail(
+                $"The service url cannot be null when using `{nameof(ClientCreationMode.DefaultAzureCredentials)}` mode."
+            );
+        }
+
+        if (!options.ServiceUri.IsAbsoluteUri)
+        {
+            return Fail(
+                $"The service url must be an absolute url when using `{nameof(ClientCreationMode.DefaultAzureCredentials)}` mode."
+            );
+        }
+
+        return Success;
+    }
+
+    private static ValidateOptionsResult ValidateModeConnectionString(
+        BlobContainerAvailableOptions options
+    )
+    {
+        if (string.IsNullOrWhiteSpace(options.ConnectionString))
+        {
+            return Fail(
+                $"The connection string cannot be null or whitespace when using `{nameof(ClientCreationMode.ConnectionString)}` mode."
+            );
+        }
+
+        return Success;
+    }
+
+    private ValidateOptionsResult ValidateModeServiceProvider()
+    {
+        if (_serviceProvider.GetService<BlobServiceClient>() is null)
+        {
+            return Fail(
+                $"No service of type `{nameof(BlobServiceClient)}` registered. Please execute `builder.AddAzureClients()`."
+            );
+        }
+
+        return Success;
+    }
+}
---
+++ src/HealthChecks.Azure.Blobs/tests/NetEvolve.HealthChecks.Azure.Blobs.Tests.Unit/DependencyInjectionExtensionsTests.cs
@@ -0,0 +1,91 @@
+﻿namespace NetEvolve.HealthChecks.Azure.Blobs.Tests.Unit;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetEvolve.Extensions.XUnit;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+[UnitTest]
+public class DependencyInjectionExtensionsTests
+{
+    [Fact]
+    public void AddAzureBlobAvailability_WhenArgumentBuilderNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var builder = default(IHealthChecksBuilder);
+
+        // Act
+        void Act() => _ = builder.AddAzureBlobAvailability("Test");
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("builder", Act);
+    }
+
+    [Fact]
+    public void AddAzureBlobAvailability_WhenArgumentNameNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var name = default(string);
+
+        // Act
+        void Act() => _ = builder.AddAzureBlobAvailability(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("name", Act);
+    }
+
+    [Fact]
+    public void AddAzureBlobAvailability_WhenArgumentNameEmpty_ThrowArgumentException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var name = string.Empty;
+
+        // Act
+        void Act() => _ = builder.AddAzureBlobAvailability(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>("name", Act);
+    }
+
+    [Fact]
+    public void AddAzureBlobAvailability_WhenArgumentTagsNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var tags = default(string[]);
+
+        // Act
+        void Act() => _ = builder.AddAzureBlobAvailability("Test", tags: tags);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("tags", Act);
+    }
+
+    [Fact]
+    public void AddAzureBlobAvailability_WhenArgumentNameIsAlreadyUsed_ThrowArgumentException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        const string name = "Test";
+
+        // Act
+        void Act() =>
+            _ = builder.AddAzureBlobAvailability(name, x => { }).AddAzureBlobAvailability(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>(nameof(name), Act);
+    }
+}
