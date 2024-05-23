35de98bac63861a54f28aa9e3f7ce1226fb8d6f1
Martin Stühmer <martin@stuehmer.net>
2024-05-23 13:10:47 +0200

    feat: Redis Implementation (#306)
    * feat: Redis Implementation
    
    * fix: Readded projects to the solution
    
    * fix: Now it's able to build
    
    * fix(style): Encoding
    
    * fix(style): Encoding

---
+++ src/HealthChecks.Redis/tests/NetEvolve.HealthChecks.Redis.Tests.Integration/RedisDatabaseCheckTests.cs
@@ -0,0 +1,186 @@
+﻿namespace NetEvolve.HealthChecks.Redis.Tests.Integration;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NetEvolve.Extensions.XUnit;
+using NetEvolve.HealthChecks.Tests;
+using StackExchange.Redis;
+using Xunit;
+
+[IntegrationTest]
+[ExcludeFromCodeCoverage]
+[SetCulture]
+public class RedisDatabaseCheckTests : HealthCheckTestBase, IClassFixture<RedisDatabase>
+{
+    private readonly RedisDatabase _database;
+
+    public RedisDatabaseCheckTests(RedisDatabase database) => _database = database;
+
+    [Fact]
+    public async Task AddRedisDatabase_UseOptionsCreate_ShouldReturnHealthy() =>
+        await RunAndVerify(healthChecks =>
+        {
+            _ = healthChecks.AddRedisDatabase(
+                "TestContainerHealthy",
+                options =>
+                {
+                    options.ConnectionString = _database.GetConnectionString();
+                    options.Mode = ConnectionHandleMode.Create;
+                }
+            );
+        });
+
+    [Fact]
+    public async Task AddRedisDatabase_UseOptionsServiceProvider_ShouldReturnHealthy() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddRedisDatabase(
+                    "TestContainerHealthy",
+                    options =>
+                    {
+                        options.ConnectionString = _database.GetConnectionString();
+                    }
+                );
+            },
+            serviceBuilder: builder =>
+            {
+                _ = builder.AddSingleton<IConnectionMultiplexer>(services =>
+                {
+                    var options = services.GetService<IOptionsSnapshot<RedisDatabaseOptions>>();
+
+                    return ConnectionMultiplexer.Connect(
+                        options!.Get("TestContainerHealthy").ConnectionString
+                    );
+                });
+            }
+        );
+
+    [Fact]
+    public async Task AddRedisDatabase_UseOptionsDoubleRegistered_ShouldReturnHealthy() =>
+        _ = await Assert.ThrowsAsync<ArgumentException>(
+            "name",
+            async () =>
+            {
+                await RunAndVerify(healthChecks =>
+                {
+                    _ = healthChecks
+                        .AddRedisDatabase("TestContainerHealthy")
+                        .AddRedisDatabase("TestContainerHealthy");
+                });
+            }
+        );
+
+    [Fact]
+    public async Task AddRedisDatabase_UseOptions_ShouldReturnDegraded() =>
+        await RunAndVerify(healthChecks =>
+        {
+            _ = healthChecks.AddRedisDatabase(
+                "TestContainerDegraded",
+                options =>
+                {
+                    options.ConnectionString = _database.GetConnectionString();
+                    options.Timeout = 0;
+                    options.Mode = ConnectionHandleMode.Create;
+                }
+            );
+        });
+
+    [Fact]
+    public async Task AddRedisDatabase_UseConfiguration_ShouldReturnHealthy() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddRedisDatabase("TestContainerHealthy");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
+                {
+                    {
+                        "HealthChecks:RedisDatabase:TestContainerHealthy:ConnectionString",
+                        _database.GetConnectionString()
+                    }
+                };
+                _ = config.AddInMemoryCollection(values);
+            },
+            serviceBuilder: builder =>
+            {
+                _ = builder.AddSingleton<IConnectionMultiplexer>(services =>
+                {
+                    var options = services.GetService<IOptionsSnapshot<RedisDatabaseOptions>>();
+
+                    return ConnectionMultiplexer.Connect(
+                        options!.Get("TestContainerHealthy").ConnectionString
+                    );
+                });
+            }
+        );
+
+    [Fact]
+    public async Task AddRedisDatabase_UseConfiguration_ShouldReturnDegraded() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddRedisDatabase("TestContainerDegraded");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
+                {
+                    {
+                        "HealthChecks:RedisDatabase:TestContainerDegraded:ConnectionString",
+                        _database.GetConnectionString()
+                    },
+                    { "HealthChecks:RedisDatabase:TestContainerDegraded:Timeout", "0" },
+                    { "HealthChecks:RedisDatabase:TestContainerDegraded:Mode", "Create" }
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Fact]
+    public async Task AddRedisDatabase_UseConfigration_ConnectionStringEmpty_ThrowException() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddRedisDatabase("TestNoValues");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
+                {
+                    { "HealthChecks:RedisDatabase:TestNoValues:ConnectionString", "" },
+                    { "HealthChecks:RedisDatabase:TestNoValues:Mode", "Create" }
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Fact]
+    public async Task AddRedisDatabase_UseConfigration_TimeoutMinusTwo_ThrowException() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddRedisDatabase("TestNoValues");
+            },
+            config =>
+            {
+                var values = new Dictionary<string, string?>
+                {
+                    {
+                        "HealthChecks:RedisDatabase:TestNoValues:ConnectionString",
+                        _database.GetConnectionString()
+                    },
+                    { "HealthChecks:RedisDatabase:TestNoValues:Timeout", "-2" },
+                    { "HealthChecks:RedisDatabase:TestNoValues:Mode", "Create" }
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+}
