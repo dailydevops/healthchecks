4c9ca670482b060e181e6e62114564322f61c865
Martin Stühmer <me@samtrion.net>
2024-12-17 14:56:27 +0100

    feat: Apache ActiveMQ package

---
+++ src/NetEvolve.HealthChecks.Apache.ActiveMq/ActiveMqConfigure.cs
@@ -0,0 +1,15 @@
+﻿namespace NetEvolve.HealthChecks.Apache.ActiveMq;
+
+using Microsoft.Extensions.Options;
+
+internal sealed class ActiveMqConfigure
+    : IConfigureNamedOptions<ActiveMqOptions>,
+        IValidateOptions<ActiveMqOptions>
+{
+    public void Configure(string? name, ActiveMqOptions options) { }
+
+    public void Configure(ActiveMqOptions options) { }
+
+    public ValidateOptionsResult Validate(string? name, ActiveMqOptions options) =>
+        ValidateOptionsResult.Success;
+}
