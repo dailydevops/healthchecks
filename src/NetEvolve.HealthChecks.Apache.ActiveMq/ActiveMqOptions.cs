b697b41755833f1133b9094bb3df36cd0e6d45c3
Martin Stühmer <me@samtrion.net>
2024-12-17 14:56:27 +0100

    feat: Apache ActiveMQ package

---
+++ src/NetEvolve.HealthChecks.Apache.ActiveMq/ActiveMqOptions.cs
@@ -0,0 +1,34 @@
+﻿namespace NetEvolve.HealthChecks.Apache.ActiveMq;
+
+public class ActiveMqOptions
+{
+    public string BrokerAddress { get; set; }
+    public string? Username
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(field))
+            {
+                return null;
+            }
+
+            return field;
+        }
+        set;
+    }
+    public string? Password
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(field))
+            {
+                return null;
+            }
+
+            return field;
+        }
+        set;
+    }
+
+    public int Timeout { get; set; } = 100;
+}
---
+++ src/NetEvolve.HealthChecks.Apache.ActiveMq/DependencyInjectionExtensions.cs
@@ -0,0 +1,67 @@
+﻿namespace NetEvolve.HealthChecks.Apache.ActiveMq;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetEvolve.Arguments;
+using NetEvolve.HealthChecks.Abstractions;
+
+/// <summary>
+/// Extensions methods for <see cref="IHealthChecksBuilder"/> with custom Health Checks.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+    private static readonly string[] _defaultTags = ["activemq", "message-queue"];
+
+    /// <summary>
+    /// Add a health check for Apache ActiveMq.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+    /// <param name="name">The name of the <see cref="ActiveMqHealthCheck"/>.</param>
+    /// <param name="options">An optional action to configure.</param>
+    /// <param name="tags">A list of additional tags that can be used to filter sets of health checks. Optional.</param>
+    /// <exception cref="ArgumentNullException">The <paramref name="builder"/> is <see langword="null" />.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="name"/> is <see langword="null" />.</exception>
+    /// <exception cref="ArgumentException">The <paramref name="name"/> is <see langword="null" /> or <c>whitespace</c>.</exception>
+    /// <exception cref="ArgumentException">The <paramref name="name"/> is already in use.</exception>
+    /// <exception cref="ArgumentNullException">The <paramref name="tags"/> is <see langword="null" />.</exception>
+    public static IHealthChecksBuilder AddActiveMq(
+        [NotNull] this IHealthChecksBuilder builder,
+        [NotNull] string name,
+        Action<ActiveMqOptions>? options = null,
+        params string[] tags
+    )
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        Argument.ThrowIfNullOrEmpty(name);
+        ArgumentNullException.ThrowIfNull(tags);
+
+        if (!builder.IsServiceTypeRegistered<ActiveMqCheckMarker>())
+        {
+            _ = builder
+                .Services.AddSingleton<ActiveMqCheckMarker>()
+                .AddSingleton<ActiveMqHealthCheck>()
+                .ConfigureOptions<ActiveMqConfigure>();
+        }
+
+        if (builder.IsNameAlreadyUsed<ActiveMqHealthCheck>(name))
+        {
+            throw new ArgumentException($"Name `{name}` already in use.", nameof(name), null);
+        }
+
+        if (options is not null)
+        {
+            _ = builder.Services.Configure(name, options);
+        }
+
+        return builder.AddCheck<ActiveMqHealthCheck>(
+            name,
+            HealthStatus.Unhealthy,
+            _defaultTags.Union(tags, StringComparer.OrdinalIgnoreCase)
+        );
+    }
+
+    private sealed partial class ActiveMqCheckMarker { }
+}
