d8bd6bd65edef8950e0410896c90fd9ec87f2ab0
Martin Stühmer <me@samtrion.net>
2025-05-04 14:08:08 +0200

    feat: Azure ServiceBus package (#559)

---
+++ src/NetEvolve.HealthChecks.Azure.ServiceBus/ServiceBusQueueOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace NetEvolve.HealthChecks.Azure.ServiceBus;
+
+using Microsoft.Extensions.Options;
+
+public class ServiceBusQueueOptions : ServiceBusOptionsBase
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether to enable peek mode, default is <c>false</c>.
+    /// </summary>
+    /// <remarks>
+    /// To enable the peek mode, the executing user requires Listen claim to work.
+    /// </remarks>
+    /// <seealso href="https://learn.microsoft.com/azure/role-based-access-control/built-in-roles#azure-service-bus-data-sender"/>
+    public bool EnablePeekMode { get; set; }
+
+    /// <summary>
+    /// Gets or sets the queue name, which is checked if it exists.
+    /// </summary>
+    public string? QueueName { get; set; }
+}
---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Azure/ServiceBus/ServiceBusQueueHealthCheckTests.cs
@@ -0,0 +1,53 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.Azure.ServiceBus;
+
+using Microsoft.Extensions.Azure;
+using NetEvolve.HealthChecks.Azure.ServiceBus;
+
+public class ServiceBusQueueHealthCheckTests
+    : HealthCheckTestBase,
+        IClassFixture<ServiceBusContainer>
+{
+    private readonly ServiceBusContainer _container;
+
+    public ServiceBusQueueHealthCheckTests(ServiceBusContainer container) => _container = container;
+
+    [Fact]
+    public async Task AddServiceBusQueueHealthCheck_UseOptions_ModeServiceProvider_ShouldReturnHealthy() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddServiceBusQueueHealthCheck(
+                    "ServiceBusQueueServiceProviderHealthy",
+                    options => options.QueueName = ServiceBusContainer.QueueName
+                );
+            },
+            serviceBuilder: services =>
+            {
+                services.AddAzureClients(clients =>
+                    _ = clients.AddServiceBusAdministrationClient(_container.ConnectionString)
+                );
+            }
+        );
+
+    [Fact]
+    public async Task AddServiceBusQueueHealthCheck_UseOptions_EnablePeekModeServiceProvider_ShouldReturnHealthy() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddServiceBusQueueHealthCheck(
+                    "ServiceBusQueueServiceProviderHealthy",
+                    options =>
+                    {
+                        options.EnablePeekMode = true;
+                        options.QueueName = ServiceBusContainer.QueueName;
+                    }
+                );
+            },
+            serviceBuilder: services =>
+            {
+                services.AddAzureClients(clients =>
+                    _ = clients.AddServiceBusClient(_container.ConnectionString)
+                );
+            }
+        );
+}
