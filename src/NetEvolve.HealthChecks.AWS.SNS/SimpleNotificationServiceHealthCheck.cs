134c7430ad19104a4df3505cb9a9955e2ec4e308
Martin Stühmer <me@samtrion.net>
2025-05-04 14:06:10 +0200

    feat: Added AWS SNS package (#573)

---
+++ src/NetEvolve.HealthChecks.AWS.SNS/SimpleNotificationServiceHealthCheck.cs
@@ -0,0 +1,69 @@
+﻿namespace NetEvolve.HealthChecks.AWS.SNS;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using NetEvolve.Extensions.Tasks;
+using NetEvolve.HealthChecks.Abstractions;
+
+internal sealed class SimpleNotificationServiceHealthCheck
+    : ConfigurableHealthCheckBase<SimpleNotificationServiceOptions>
+{
+    public SimpleNotificationServiceHealthCheck(
+        IOptionsMonitor<SimpleNotificationServiceOptions> optionsMonitor
+    )
+        : base(optionsMonitor) { }
+
+    protected override async ValueTask<HealthCheckResult> ExecuteHealthCheckAsync(
+        string name,
+        HealthStatus failureStatus,
+        SimpleNotificationServiceOptions options,
+        CancellationToken cancellationToken
+    )
+    {
+        using var client = CreateClient(options);
+
+        var (isValid, topic) = await client
+            .GetSubscriptionAttributesAsync(
+                new GetSubscriptionAttributesRequest { SubscriptionArn = options.TopicName },
+                cancellationToken
+            )
+            .WithTimeoutAsync(options.Timeout, cancellationToken)
+            .ConfigureAwait(false);
+
+        return HealthCheckState(isValid && topic is not null, name);
+    }
+
+    private static AmazonSimpleNotificationServiceClient CreateClient(
+        SimpleNotificationServiceOptions options
+    )
+    {
+        var hasCredentials = options.GetCredentials() is not null;
+        var hasEndpoint = options.GetRegionEndpoint() is not null;
+
+        var config = new AmazonSimpleNotificationServiceConfig
+        {
+            ServiceURL = options.ServiceUrl,
+            RegionEndpoint = RegionEndpoint.USEast1,
+        };
+
+        return (hasCredentials, hasEndpoint) switch
+        {
+            (true, true) => new AmazonSimpleNotificationServiceClient(
+                options.GetCredentials(),
+                options.GetRegionEndpoint()
+            ),
+            (true, false) => new AmazonSimpleNotificationServiceClient(
+                options.GetCredentials(),
+                config
+            ),
+            (false, true) => new AmazonSimpleNotificationServiceClient(options.GetRegionEndpoint()),
+            _ => throw new InvalidOperationException("Invalid ClientCreationMode."),
+        };
+    }
+}
---
+++ tests/NetEvolve.HealthChecks.Tests.Unit/Apache/ActiveMq/KafkaConfigureTests.cs
@@ -0,0 +1,86 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Unit.Apache.ActiveMq;
+
+using System;
+using Microsoft.Extensions.Configuration;
+using NetEvolve.HealthChecks.Apache.ActiveMq;
+using Xunit;
+
+public sealed class ActiveMqConfigureTests
+{
+    [Fact]
+    public void Validate_WhenArgumentNameNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var options = new ActiveMqOptions();
+        var configure = new ActiveMqConfigure(new ConfigurationBuilder().Build());
+        var name = default(string);
+
+        // Act
+        var result = configure.Validate(name, options);
+
+        // Assert
+        Assert.True(result.Failed);
+        Assert.Equal("The name cannot be null or whitespace.", result.FailureMessage);
+    }
+
+    [Fact]
+    public void Validate_WhenArgumentOptionsNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configure = new ActiveMqConfigure(new ConfigurationBuilder().Build());
+        var name = "Test";
+        var options = default(ActiveMqOptions);
+
+        // Act
+        var result = configure.Validate(name, options);
+
+        // Assert
+        Assert.True(result.Failed);
+        Assert.Equal("The option cannot be null.", result.FailureMessage);
+    }
+
+    [Fact]
+    public void Validate_WhenArgumentConnectionStringNull_ThrowArgumentException()
+    {
+        // Arrange
+        var configure = new ActiveMqConfigure(new ConfigurationBuilder().Build());
+        var name = "Test";
+        var options = new ActiveMqOptions();
+
+        // Act
+        var result = configure.Validate(name, options);
+
+        // Assert
+        Assert.True(result.Failed);
+        Assert.Equal("The broker address cannot be null or whitespace.", result.FailureMessage);
+    }
+
+    [Fact]
+    public void Configure_WhenArgumentNameNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configure = new ActiveMqConfigure(new ConfigurationBuilder().Build());
+        var name = default(string);
+        var options = new ActiveMqOptions();
+
+        // Act
+        void Act() => configure.Configure(name, options);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("name", Act);
+    }
+
+    [Fact]
+    public void Configure_WhenArgumentOptionsNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configure = new ActiveMqConfigure(new ConfigurationBuilder().Build());
+        var options = new ActiveMqOptions();
+
+        // Act
+        void Act() => configure.Configure(options);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>("name", Act);
+    }
+}
