79f268eb552ce121888f26e4a57a457fce17f06f
Martin Stühmer <me@samtrion.net>
2025-05-04 14:09:35 +0200

    feat: Azure CosmosDB package (#562)

---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Azure/Cosmos/CosmosDbContainer.cs
@@ -0,0 +1,18 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.Azure.Cosmos;
+
+using System;
+using System.Threading.Tasks;
+using Testcontainers.CosmosDb;
+using TestContainer = Testcontainers.CosmosDb.CosmosDbContainer;
+
+public sealed class CosmosDbContainer : IAsyncLifetime, IAsyncDisposable
+{
+    private readonly TestContainer _container = new CosmosDbBuilder().Build();
+
+    public async Task DisposeAsync() => await _container.DisposeAsync().ConfigureAwait(false);
+
+    public async Task InitializeAsync() => await _container.StartAsync().ConfigureAwait(false);
+
+    async ValueTask IAsyncDisposable.DisposeAsync() =>
+        await _container.DisposeAsync().ConfigureAwait(false);
+}
