fc29b6d719ac10044b1e5ec7a0f37ca53dbbecc6
Martin Stühmer <me@samtrion.net>
2025-05-05 18:48:04 +0200

    chore: Cleanup SNS

--- tests/NetEvolve.HealthChecks.Tests.Integration/AWS/SNS/SimpleNotificationServiceHealthCheckTests.cs
+++ tests/NetEvolve.HealthChecks.Tests.Integration/AWS/SNS/SimpleNotificationServiceHealthCheckTests.cs
@@ -2,17 +2,13 @@
 
 using NetEvolve.Extensions.XUnit;
 using NetEvolve.HealthChecks.AWS.SNS;
-using NodaTime;
 
 [TestGroup($"{nameof(AWS)}.{nameof(SNS)}")]
 public class SimpleNotificationServiceHealthCheckTests : HealthCheckTestBase, IClassFixture<LocalStackInstance>
 {
     private readonly LocalStackInstance _instance;
 
-    public SimpleNotificationServiceHealthCheckTests(LocalStackInstance instance)
-    {
-        _instance = instance;
-    }
+    public SimpleNotificationServiceHealthCheckTests(LocalStackInstance instance) => _instance = instance;
 
     [Fact]
     public async Task AddSimpleNotificationService_UseOptionsCreate_ShouldReturnHealthy() =>
