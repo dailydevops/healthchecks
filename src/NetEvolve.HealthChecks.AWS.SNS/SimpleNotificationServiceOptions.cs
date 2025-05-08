1b775794418fb9b9743039dd8ec3267ed4080dd0
Martin Stühmer <me@samtrion.net>
2025-05-08 22:48:30 +0200

    chore: Extended the tests for `AWS.SNS`

--- src/NetEvolve.HealthChecks.AWS.SNS/SimpleNotificationServiceOptions.cs
+++ src/NetEvolve.HealthChecks.AWS.SNS/SimpleNotificationServiceOptions.cs
@@ -7,24 +7,27 @@ public sealed class SimpleNotificationServiceOptions
 {
     public string? AccessKey { get; set; }
 
-    public CreationMode CreationMode { get; set; }
+    public CreationMode? Mode { get; set; }
 
     public string? SecretKey { get; set; }
 
 #pragma warning disable CA1056 // URI-like properties should not be strings
+    /// <inheritdoc cref="ClientConfig.ServiceURL"/>
     public string? ServiceUrl { get; set; }
 #pragma warning restore CA1056 // URI-like properties should not be strings
 
     public string? TopicName { get; set; }
 
     public int Timeout { get; set; } = 100;
+    public string? Subscription { get; set; }
+
+    /// <inheritdoc cref="ClientConfig.RegionEndpoint"/>
+    public RegionEndpoint? RegionEndpoint { get; set; }
 
     internal AWSCredentials? GetCredentials() =>
-        CreationMode switch
+        Mode switch
         {
             CreationMode.BasicAuthentication => new BasicAWSCredentials(AccessKey, SecretKey),
             _ => null,
         };
-
-    internal RegionEndpoint? GetRegionEndpoint() => CreationMode == CreationMode ? null : null;
 }
