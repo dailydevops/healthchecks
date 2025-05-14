b0905186fdb960b85badd5fab31e0ce7a3410788
Martin Stühmer <me@samtrion.net>
2025-05-14 11:02:57 +0200

    chore: Added Validation for `SimpleNotificationService`

--- src/NetEvolve.HealthChecks.AWS.SNS/SimpleNotificationServiceConfigure.cs
+++ src/NetEvolve.HealthChecks.AWS.SNS/SimpleNotificationServiceConfigure.cs
@@ -10,6 +10,40 @@ internal sealed class SimpleNotificationServiceConfigure
 
     public void Configure(SimpleNotificationServiceOptions options) { }
 
-    public ValidateOptionsResult Validate(string? name, SimpleNotificationServiceOptions options) =>
-        ValidateOptionsResult.Success;
+    public ValidateOptionsResult Validate(string? name, SimpleNotificationServiceOptions options)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return ValidateOptionsResult.Fail("The name cannot be null or whitespace.");
+        }
+
+        if (options is null)
+        {
+            return ValidateOptionsResult.Fail("The option cannot be null.");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.TopicName))
+        {
+            return ValidateOptionsResult.Fail("The topic name cannot be null or whitespace.");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.ServiceUrl))
+        {
+            return ValidateOptionsResult.Fail("The service URL cannot be null or whitespace.");
+        }
+
+        if (options.Mode is CreationMode.BasicAuthentication)
+        {
+            if (string.IsNullOrWhiteSpace(options.AccessKey))
+            {
+                return ValidateOptionsResult.Fail("The access key cannot be null or whitespace.");
+            }
+            if (string.IsNullOrWhiteSpace(options.SecretKey))
+            {
+                return ValidateOptionsResult.Fail("The secret key cannot be null or whitespace.");
+            }
+        }
+
+        return ValidateOptionsResult.Success;
+    }
 }
