b3bacaa96da20f5d01c8588d7cdddc667f269e1f
Martin Stühmer <me@samtrion.net>
2025-05-21 00:10:53 +0200

    fix: Added missing xml documentations

--- src/NetEvolve.HealthChecks.Azure.ServiceBus/ServiceBusQueueOptions.cs
+++ src/NetEvolve.HealthChecks.Azure.ServiceBus/ServiceBusQueueOptions.cs
@@ -1,18 +1,21 @@
 ﻿namespace NetEvolve.HealthChecks.Azure.ServiceBus;
 
+/// <summary>
+/// Represents configuration options for Azure Service Bus queue health checks.
+/// </summary>
 public class ServiceBusQueueOptions : ServiceBusOptionsBase
 {
     /// <summary>
-    /// Gets or sets a value indicating whether to enable peek mode, default is <c>false</c>.
+    /// Gets or sets a value indicating whether to enable peek mode. Default is <c>false</c>.
     /// </summary>
     /// <remarks>
-    /// To enable the peek mode, the executing user requires Listen claim to work.
+    /// To enable the peek mode, the executing user requires the Listen claim to work.
     /// </remarks>
     /// <seealso href="https://learn.microsoft.com/azure/role-based-access-control/built-in-roles#azure-service-bus-data-sender"/>
     public bool EnablePeekMode { get; set; }
 
     /// <summary>
-    /// Gets or sets the queue name, which is checked if it exists.
+    /// Gets or sets the name of the queue to check for existence.
     /// </summary>
     public string? QueueName { get; set; }
 }
