3e3cda2dc511e87003aa9c92b43cabc67aca9792
Martin Stühmer <me@samtrion.net>
2025-05-22 21:44:08 +0200

    fix(test): Added missing testcase for `HealthCheckBase`

---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/HealthChecks/HealthCheckBaseTests.cs
@@ -0,0 +1,31 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.HealthChecks;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetEvolve.HealthChecks.Abstractions;
+
+public class HealthCheckBaseTests : HealthCheckTestBase
+{
+    [Fact]
+    public async Task ExecuteHealthCheckAsync_ShouldReturnUnhealthy() =>
+        await RunAndVerify(healthChecks =>
+            healthChecks.Add(
+                new HealthCheckRegistration(
+                    "NotImplementedException",
+                    new TestHealthCheck(),
+                    HealthStatus.Unhealthy,
+                    null
+                )
+            )
+        );
+
+    private sealed class TestHealthCheck : HealthCheckBase
+    {
+        protected override ValueTask<HealthCheckResult> ExecuteHealthCheckAsync(
+            string name,
+            HealthStatus failureStatus,
+            CancellationToken cancellationToken
+        ) => throw new NotImplementedException();
+    }
+}
