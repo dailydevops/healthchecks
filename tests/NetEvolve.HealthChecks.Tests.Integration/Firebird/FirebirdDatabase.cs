4f3b8928c1b73ef0777bc516913775ec03bf7703
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-05-22 19:55:03 +0200

    feat(firebird): aded firebird checks

---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Firebird/FirebirdDatabase.cs
@@ -0,0 +1,15 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.Firebird;
+
+using System.Threading.Tasks;
+using Testcontainers.FirebirdSql;
+
+public sealed class FirebirdDatabase : IAsyncLifetime
+{
+    private readonly FirebirdSqlContainer _database = new FirebirdSqlBuilder().Build();
+
+    public string ConnectionString => _database.GetConnectionString();
+
+    public async Task DisposeAsync() => await _database.DisposeAsync().ConfigureAwait(false);
+
+    public async Task InitializeAsync() => await _database.StartAsync().ConfigureAwait(false);
+}
