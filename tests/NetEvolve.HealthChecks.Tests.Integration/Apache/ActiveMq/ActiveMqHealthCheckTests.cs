b03950a3743f129d576925539b4a27350a676bec
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-05-24 13:35:57 +0200

    test: Added `ActiveMq` integration tests

--- tests/NetEvolve.HealthChecks.Tests.Integration/Apache/ActiveMq/ActiveMqHealthCheckTests.cs
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Apache/ActiveMq/ActiveMqHealthCheckTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace NetEvolve.HealthChecks.Tests.Integration.Apache.ActiveMq;
 
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using NetEvolve.Extensions.XUnit;
 using NetEvolve.HealthChecks.Apache.ActiveMq;
 using Testcontainers.ActiveMq;
@@ -25,7 +26,7 @@ public abstract class ActiveMqHealthCheckTests : HealthCheckTestBase, IAsyncLife
     public async Task InitializeAsync() => await _container.StartAsync().ConfigureAwait(false);
 
     [Fact]
-    public async Task AddActiveMq_UseOptionsCreate_ShouldReturnHealthy() =>
+    public async Task AddActiveMq_UseOptions_ShouldReturnHealthy() =>
         await RunAndVerify(healthChecks =>
         {
             _ = healthChecks.AddActiveMq(
@@ -39,6 +40,120 @@ public abstract class ActiveMqHealthCheckTests : HealthCheckTestBase, IAsyncLife
                 }
             );
         });
+
+    [Fact]
+    public async Task AddActiveMq_UseOptions_ShouldReturnUnhealthy() =>
+        await RunAndVerify(healthChecks =>
+        {
+            _ = healthChecks.AddActiveMq(
+                "TestContainerUnhealthy",
+                options =>
+                {
+                    options.BrokerAddress = "tcp://localhost:65535"; // Unused port
+                    options.Username = "invalid";
+                    options.Password = "invalid";
+                    options.Timeout = 500;
+                }
+            );
+        });
+
+    [Fact]
+    public async Task AddActiveMq_UseOptions_ShouldReturnDegraded() =>
+        await RunAndVerify(healthChecks =>
+        {
+            _ = healthChecks.AddActiveMq(
+                "TestContainerDegraded",
+                options =>
+                {
+                    options.BrokerAddress = _container.GetBrokerAddress();
+                    options.Username = _username;
+                    options.Password = _password;
+                    options.Timeout = 0;
+                }
+            );
+        });
+
+    [Fact]
+    public async Task AddActiveMq_UseConfiguration_ShouldReturnHealthy() =>
+        await RunAndVerify(
+            healthChecks => _ = healthChecks.AddActiveMq("TestContainerHealthy"),
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:ActiveMq:TestContainerHealthy:BrokerAddress", _container.GetBrokerAddress() },
+                    { "HealthChecks:ActiveMq:TestContainerHealthy:Username", _username },
+                    { "HealthChecks:ActiveMq:TestContainerHealthy:Password", _password },
+                    { "HealthChecks:ActiveMq:TestContainerHealthy:Timeout", "500" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Fact]
+    public async Task AddActiveMq_UseConfiguration_ShouldReturnUnhealthy() =>
+        await RunAndVerify(
+            healthChecks => _ = healthChecks.AddActiveMq("TestContainerUnhealthy"),
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:ActiveMq:TestContainerUnhealthy:BrokerAddress", "tcp://localhost:65535" },
+                    { "HealthChecks:ActiveMq:TestContainerUnhealthy:Username", "invalid" },
+                    { "HealthChecks:ActiveMq:TestContainerUnhealthy:Password", "invalid" },
+                    { "HealthChecks:ActiveMq:TestContainerUnhealthy:Timeout", "500" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Fact]
+    public async Task AddActiveMq_UseConfiguration_ShouldReturnDegraded() =>
+        await RunAndVerify(
+            healthChecks => _ = healthChecks.AddActiveMq("TestContainerDegraded"),
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:ActiveMq:TestContainerDegraded:BrokerAddress", _container.GetBrokerAddress() },
+                    { "HealthChecks:ActiveMq:TestContainerDegraded:Username", _username },
+                    { "HealthChecks:ActiveMq:TestContainerDegraded:Password", _password },
+                    { "HealthChecks:ActiveMq:TestContainerDegraded:Timeout", "0" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Fact]
+    public async Task AddActiveMq_UseConfigration_BrokerAddressStringEmpty_ThrowException() =>
+        await RunAndVerify(
+            healthChecks => _ = healthChecks.AddActiveMq("TestNoValues"),
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:ActiveMq:TestNoValues:BrokerAddress", "" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Fact]
+    public async Task AddActiveMq_UseConfigration_TimeoutMinusTwo_ThrowException() =>
+        await RunAndVerify(
+            healthChecks => _ = healthChecks.AddActiveMq("TestNoValues"),
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:ActiveMq:TestNoValues:BrokerAddress", _container.GetBrokerAddress() },
+                    { "HealthChecks:ActiveMq:TestNoValues:Username", _username },
+                    { "HealthChecks:ActiveMq:TestNoValues:Password", _password },
+                    { "HealthChecks:ActiveMq:TestNoValues:Timeout", "-2" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
 
     public sealed class CustomCredentials : ActiveMqHealthCheckTests
     {
