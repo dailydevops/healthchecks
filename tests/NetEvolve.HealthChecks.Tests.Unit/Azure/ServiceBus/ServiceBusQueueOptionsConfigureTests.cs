5be659da1e004560c7f1f540fdcf58632f6d9e21
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-05-24 17:26:28 +0200

    chore: ActiveMq cleanup and completion (#708)
    * test: Added client factory tests
    
    * chore: Added configure timeout validation
    
    * chore: Renamed `ActiveMq` checks files
    
    * test: Added `ActiveMq` integration tests
    
    * chore(ActiveMq): Using const instead of var for local variables
    
    * chore(Redpanda): Use const for local variables
    
    * chore(Firebird): Use const for local variables
    
    * chore(ServiceBus): Use const for local variables

--- tests/NetEvolve.HealthChecks.Tests.Unit/Azure/ServiceBus/ServiceBusQueueOptionsConfigureTests.cs
+++ tests/NetEvolve.HealthChecks.Tests.Unit/Azure/ServiceBus/ServiceBusQueueOptionsConfigureTests.cs
@@ -13,7 +13,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = default(string);
+        const string? name = default;
         var options = new ServiceBusQueueOptions();
 
         // Act
@@ -43,7 +43,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = " ";
+        const string name = " ";
         var options = new ServiceBusQueueOptions();
 
         // Act
@@ -72,7 +72,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = default(string);
+        const string? name = default;
         var options = new ServiceBusQueueOptions();
 
         // Act
@@ -88,7 +88,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = default(ServiceBusQueueOptions);
 
         // Act
@@ -104,7 +104,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = new ServiceBusQueueOptions { Timeout = -2 };
 
         // Act
@@ -120,7 +120,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = new ServiceBusQueueOptions { Timeout = 100, Mode = null };
 
         // Act
@@ -136,7 +136,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = new ServiceBusQueueOptions
         {
             Timeout = 100,
@@ -160,7 +160,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = new ServiceBusQueueOptions
         {
             Timeout = 100,
@@ -184,7 +184,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = new ServiceBusQueueOptions
         {
             Timeout = 100,
@@ -205,7 +205,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = new ServiceBusQueueOptions
         {
             Timeout = 100,
@@ -226,7 +226,7 @@ public sealed class ServiceBusQueueOptionsConfigureTests
     {
         // Arrange
         var configure = new ServiceBusQueueOptionsConfigure(new ConfigurationBuilder().Build());
-        var name = "Test";
+        const string name = "Test";
         var options = new ServiceBusQueueOptions
         {
             Timeout = 100,
