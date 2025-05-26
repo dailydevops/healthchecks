d16c8ee1976c7b212b9f951dc61affea0e3da948
Martin Stühmer <me@samtrion.net>
2025-05-26 22:56:17 +0200

    chore: Updated script

--- scripts/update-readme.ps1
+++ scripts/update-readme.ps1
@@ -7,19 +7,32 @@ param (
 function Get-Packages {
   param (
   )
-  $repositoryUrl = 'https://github.com/dailydevops/healthchecks'
-  $queryUrl = 'https://azuresearch-usnc.nuget.org/query?q=netevolve.healthchecks&prerelease=true&semVerLevel=2.0.0'
-  $response = Invoke-WebRequest -Uri $queryUrl
-  if ($response.statuscode -ne 200) {
-    Write-Error "Failed to get packages from $repositoryUrl"
-    return
-  }
+  Write-Host "Getting packages from Nuget"
 
-  $data = ConvertFrom-Json $response.Content
-  if ($data.totalHits -eq 0) {
-    Write-Error "No packages found at $repositoryUrl"
-    return
-  }
+  $resultData = @()
+  $take = 100
+  $skip = 0
+  $repositoryUrl = 'https://github.com/dailydevops/healthchecks'
+
+  do {
+    $queryUrl = "https://azuresearch-usnc.nuget.org/query?q=netevolve.healthchecks&take=$($take)&skip=$($skip)&prerelease=true&semVerLevel=2.0.0"
+    $response = Invoke-WebRequest -Uri $queryUrl
+    if ($response.statuscode -ne 200) {
+      Write-Error "Failed to get packages from $repositoryUrl"
+      return
+    }
+
+    $data = ConvertFrom-Json $response.Content
+    if ($data.totalHits -eq 0) {
+      Write-Error "No packages found at $repositoryUrl"
+      return
+    }
+
+    $skip += $data.data.Length
+    $resultData += $data.data
+
+  } while ($skip -lt $data.totalHits)
+
 
   $result = @"
 
@@ -28,7 +41,7 @@ function Get-Packages {
 
 "@
 
-  foreach ($package in ($data.data | Sort-Object -Property id)) {
+  foreach ($package in ($resultData | Sort-Object -Property id)) {
     if ($package.projectUrl -ne $repositoryUrl) {
       continue
     }
