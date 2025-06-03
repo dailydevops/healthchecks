b625be776f42c21d9cca54da41a471afc31daeab
Martin Stühmer <me@samtrion.net>
2025-06-03 09:14:02 +0200

    test: Added a new test

---
+++ tests/NetEvolve.HealthChecks.Tests.Unit/Odbc/OdbcOptionsTests.cs
@@ -0,0 +1,17 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Unit.Odbc;
+
+using NetEvolve.Extensions.TUnit;
+using NetEvolve.HealthChecks.Odbc;
+
+[TestGroup(nameof(Odbc))]
+public class OdbcOptionsTests
+{
+    [Test]
+    public async Task Options_NotTheSame_Expected()
+    {
+        var options1 = new OdbcOptions { ConnectionString = "test" };
+        var options2 = options1 with { };
+
+        _ = await Assert.That(options1).IsEqualTo(options2).And.IsNotSameReferenceAs(options2);
+    }
+}
