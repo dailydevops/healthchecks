8a4a1c9355a2a85fca91f4d7d7818234931a35eb
Martin Stühmer <me@samtrion.net>
2025-06-04 06:55:25 +0200

    style: Reformatted code

--- tests/NetEvolve.HealthChecks.Tests.Integration/PublicApiTests.cs
+++ tests/NetEvolve.HealthChecks.Tests.Integration/PublicApiTests.cs
@@ -39,11 +39,13 @@ public class PublicApiTests
     }
 
     public static Func<Assembly>[] GetAssemblies() =>
-        [.. Assembly
-            .GetExecutingAssembly()!
-            .GetReferencedAssemblies()
-            .Where(a => a.Name?.StartsWith("NetEvolve.HealthChecks", StringComparison.OrdinalIgnoreCase) == true)
-            .Select<AssemblyName, Func<Assembly>>(a => () => Assembly.Load(a))];
+        [
+            .. Assembly
+                .GetExecutingAssembly()!
+                .GetReferencedAssemblies()
+                .Where(a => a.Name?.StartsWith("NetEvolve.HealthChecks", StringComparison.OrdinalIgnoreCase) == true)
+                .Select<AssemblyName, Func<Assembly>>(a => () => Assembly.Load(a)),
+        ];
 
     private static bool IsVisibleToIntelliSense(Type type)
     {
