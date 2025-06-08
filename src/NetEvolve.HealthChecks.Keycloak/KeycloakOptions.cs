32bb8a9542eb5c7133265ff8bef2108b7ef190db
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-06-08 23:40:02 +0200

    docs(Keycloak): Improved options XML comments

--- src/NetEvolve.HealthChecks.Keycloak/KeycloakOptions.cs
+++ src/NetEvolve.HealthChecks.Keycloak/KeycloakOptions.cs
@@ -10,6 +10,9 @@ public sealed record KeycloakOptions
     /// <summary>
     /// Gets or sets the mode used to create a client instance.
     /// </summary>
+    /// <remarks>
+    /// Defaults to <see cref="KeycloakClientCreationMode.ServiceProvider"/>.
+    /// </remarks>
     public KeycloakClientCreationMode Mode { get; set; } = KeycloakClientCreationMode.ServiceProvider;
 
     /// <summary>
@@ -25,21 +28,33 @@ public sealed record KeycloakOptions
     /// <summary>
     /// Gets or sets the address to the Keycloak instance to check.
     /// </summary>
+    /// <remarks>
+    /// This option is only required when <see cref="Mode"/> is set to <see cref="KeycloakClientCreationMode.Internal"/>.
+    /// </remarks>
     public string? BaseAddress { get; set; }
 
     /// <summary>
     /// Gets or sets the username for authenticating with the client.
     /// </summary>
+    /// <remarks>
+    /// This option is only required when <see cref="Mode"/> is set to <see cref="KeycloakClientCreationMode.Internal"/>.
+    /// </remarks>
     public string? Username { get; set; }
 
     /// <summary>
     /// Gets or sets the password for authenticating with the client.
     /// </summary>
+    /// <remarks>
+    /// This option is only required when <see cref="Mode"/> is set to <see cref="KeycloakClientCreationMode.Internal"/>.
+    /// </remarks>
     public string? Password { get; set; }
 
     /// <summary>
     /// Gets or sets the timeout to use when connecting and executing tasks against the service.
     /// </summary>
+    /// <remarks>
+    /// Defaults to <c>100 milliseconds</c>.
+    /// </remarks>
     public int Timeout { get; set; } = 100;
 
     /// <summary>
