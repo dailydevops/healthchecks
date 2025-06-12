9bc156a058831a0d5bf4b7a7ebb0e8181c771afb
Martin Stühmer <me@samtrion.net>
2025-06-12 11:58:42 +0200

    docs: Updated all SQL options with qualified XML summaries

--- src/NetEvolve.HealthChecks.DB2/DB2Options.cs
+++ src/NetEvolve.HealthChecks.DB2/DB2Options.cs
@@ -1,18 +1,34 @@
 ﻿namespace NetEvolve.HealthChecks.DB2;
 
-using NetEvolve.HealthChecks.Abstractions;
-
 /// <summary>
 /// Options for <see cref="DB2HealthCheck"/>
 /// </summary>
-public sealed record DB2Options : ISqlCheckOptions
+public sealed record DB2Options
 {
-    /// <inheritdoc cref="ISqlCheckOptions.ConnectionString"/>
+    /// <summary>
+    /// Gets or sets the connection string for the IBM DB2 database to check.
+    /// </summary>
+    /// <value>
+    /// The connection string used to establish a connection to the IBM DB2 database.
+    /// </value>
     public string ConnectionString { get; set; } = default!;
 
-    /// <inheritdoc cref="ISqlCheckOptions.Timeout"/>
+    /// <summary>
+    /// Gets or sets the timeout in milliseconds to use when connecting and executing tasks against the IBM DB2 database.
+    /// </summary>
+    /// <value>
+    /// The timeout in milliseconds. Default value is 100 milliseconds.
+    /// </value>
     public int Timeout { get; set; } = 100;
 
-    /// <inheritdoc cref="ISqlCheckOptions.Command"/>
+    /// <summary>
+    /// Gets the SQL command to execute against the IBM DB2 database.
+    /// </summary>
+    /// <value>
+    /// The SQL command to execute. Default value is defined by <see cref="DB2HealthCheck.DefaultCommand"/>.
+    /// </value>
+    /// <remarks>
+    /// This property is for internal use only.
+    /// </remarks>
     public string Command { get; internal set; } = DB2HealthCheck.DefaultCommand;
 }
--- src/NetEvolve.HealthChecks.SqlServer/SqlServerOptions.cs
+++ src/NetEvolve.HealthChecks.SqlServer/SqlServerOptions.cs
@@ -1,18 +1,34 @@
 ﻿namespace NetEvolve.HealthChecks.SqlServer;
 
-using NetEvolve.HealthChecks.Abstractions;
-
 /// <summary>
 /// Options for <see cref="SqlServerHealthCheck"/>
 /// </summary>
-public sealed record SqlServerOptions : ISqlCheckOptions
+public sealed record SqlServerOptions
 {
-    /// <inheritdoc cref="ISqlCheckOptions.ConnectionString"/>
+    /// <summary>
+    /// Gets or sets the connection string for the SQL Server database to check.
+    /// </summary>
+    /// <value>
+    /// The connection string used to establish a connection to the SQL Server database.
+    /// </value>
     public string ConnectionString { get; set; } = default!;
 
-    /// <inheritdoc cref="ISqlCheckOptions.Timeout"/>
+    /// <summary>
+    /// Gets or sets the timeout in milliseconds to use when connecting and executing tasks against the SQL Server database.
+    /// </summary>
+    /// <value>
+    /// The timeout in milliseconds. Default value is 100 milliseconds.
+    /// </value>
     public int Timeout { get; set; } = 100;
 
-    /// <inheritdoc cref="ISqlCheckOptions.Command"/>
+    /// <summary>
+    /// Gets the SQL command to execute against the SQL Server database.
+    /// </summary>
+    /// <value>
+    /// The SQL command to execute. Default value is defined by <see cref="SqlServerHealthCheck.DefaultCommand"/>.
+    /// </value>
+    /// <remarks>
+    /// This property is for internal use only.
+    /// </remarks>
     public string Command { get; internal set; } = SqlServerHealthCheck.DefaultCommand;
 }
