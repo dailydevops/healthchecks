c4205b727cb1c03209c979b8748d7538f244f068
Martin Stühmer <me@samtrion.net>
2025-06-15 12:52:32 +0200

    feat: Added Implementation `NetEvolve.HealthChecks.SqlServer.Devart`
    Closed: #815

---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/SqlServer.Devart/SqlServerHealthCheckTests.cs
@@ -0,0 +1,133 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.SqlServer.Devart;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetEvolve.Extensions.TUnit;
+using NetEvolve.HealthChecks.SqlServer.Devart;
+
+[TestGroup($"{nameof(SqlServer)}.{nameof(Devart)}")]
+[ClassDataSource<SqlServerDatabase>(Shared = SharedType.PerTestSession)]
+public class SqlServerHealthCheckTests : HealthCheckTestBase
+{
+    private readonly SqlServerDatabase _database;
+
+    public SqlServerHealthCheckTests(SqlServerDatabase database) => _database = database;
+
+    [Test]
+    public async Task AddSqlServerDevart_UseOptions_Healthy() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddSqlServerDevart(
+                    "TestContainerHealthy",
+                    options =>
+                    {
+                        options.ConnectionString = _database.ConnectionString;
+                        options.Timeout = 1000; // Set a reasonable timeout
+                    }
+                );
+            },
+            HealthStatus.Healthy
+        );
+
+    [Test]
+    public async Task AddSqlServerDevart_UseOptions_Degraded() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddSqlServerDevart(
+                    "TestContainerDegraded",
+                    options =>
+                    {
+                        options.ConnectionString = _database.ConnectionString;
+                        options.Command = "SELECT 1; WAITFOR DELAY '00:00:00.100';";
+                        options.Timeout = 0;
+                    }
+                );
+            },
+            HealthStatus.Degraded
+        );
+
+    [Test]
+    public async Task AddSqlServerDevart_UseOptions_Unhealthy() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddSqlServerDevart(
+                    "TestContainerUnhealthy",
+                    options =>
+                    {
+                        options.ConnectionString = _database.ConnectionString;
+                        options.Command = "RAISERROR('This is a test.',16,1)";
+                    }
+                );
+            },
+            HealthStatus.Unhealthy
+        );
+
+    [Test]
+    public async Task AddSqlServerDevart_UseConfiguration_Healthy() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddSqlServerDevart("TestContainerHealthy"),
+            HealthStatus.Healthy,
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:SqlServer:TestContainerHealthy:ConnectionString", _database.ConnectionString },
+                    { "HealthChecks:SqlServer:TestContainerHealthy:Timeout", "1000" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Test]
+    public async Task AddSqlServerDevart_UseConfiguration_Degraded() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddSqlServerDevart("TestContainerDegraded"),
+            HealthStatus.Degraded,
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:SqlServer:TestContainerDegraded:ConnectionString", _database.ConnectionString },
+                    { "HealthChecks:SqlServer:TestContainerDegraded:Timeout", "0" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Test]
+    public async Task AddSqlServerDevart_UseConfiguration_ConnectionStringEmpty_ThrowException() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddSqlServerDevart("TestNoValues"),
+            HealthStatus.Unhealthy,
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:SqlServer:TestNoValues:ConnectionString", "" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Test]
+    public async Task AddSqlServerDevart_UseConfiguration_TimeoutMinusTwo_ThrowException() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddSqlServerDevart("TestNoValues"),
+            HealthStatus.Unhealthy,
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:SqlServer:TestNoValues:ConnectionString", _database.ConnectionString },
+                    { "HealthChecks:SqlServer:TestNoValues:Timeout", "-2" },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+}
