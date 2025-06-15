452cb52d5d1b592a71e58c95e685dec8c89af569
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-06-15 16:16:03 +0200

    feat(test): Added support for Elasticsearch (Testcontainer v4.5.0)

---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Elasticsearch/Container/ContainerDefaultPassword.cs
@@ -0,0 +1,9 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.Elasticsearch.Container;
+
+using Testcontainers.Elasticsearch;
+
+public sealed class ContainerDefaultPassword : ContainerBase
+{
+    public ContainerDefaultPassword()
+        : base(ElasticsearchBuilder.DefaultPassword) { }
+}
---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Elasticsearch/ElasticsearchNoPasswordHealthCheckTests.cs
@@ -0,0 +1,13 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.Elasticsearch;
+
+using NetEvolve.Extensions.TUnit;
+using NetEvolve.HealthChecks.Tests.Integration.Elasticsearch.Container;
+
+[ClassDataSource<ContainerNoPassword>(Shared = SharedType.PerTestSession)]
+[InheritsTests]
+[TestGroup(nameof(Elasticsearch))]
+public sealed class ElasticsearchNoPasswordHealthCheckTests : ElasticsearchHealthCheckBaseTests
+{
+    public ElasticsearchNoPasswordHealthCheckTests(ContainerNoPassword container)
+        : base(container) { }
+}
