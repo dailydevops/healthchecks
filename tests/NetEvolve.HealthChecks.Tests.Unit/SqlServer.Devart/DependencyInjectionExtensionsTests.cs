efcfbd059be62520c5dc7b859422f282d9f3b994
Martin Stühmer <me@samtrion.net>
2025-06-15 12:52:32 +0200

    feat: Added Implementation `NetEvolve.HealthChecks.SqlServer.Devart`
    Closed: #815

---
+++ tests/NetEvolve.HealthChecks.Tests.Unit/SqlServer.Devart/DependencyInjectionExtensionsTests.cs
@@ -0,0 +1,88 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Unit.SqlServer.Devart;
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetEvolve.Extensions.TUnit;
+using NetEvolve.HealthChecks.SqlServer.Devart;
+
+[TestGroup($"{nameof(SqlServer)}.{nameof(Devart)}")]
+public class DependencyInjectionExtensionsTests
+{
+    [Test]
+    public void AddSqlServerDevart_WhenArgumentBuilderNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var builder = default(IHealthChecksBuilder);
+
+        // Act
+        void Act() => builder.AddSqlServerDevart("Test");
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("builder", Act);
+    }
+
+    [Test]
+    public void AddSqlServerDevart_WhenArgumentNameNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        const string? name = default;
+
+        // Act
+        void Act() => builder.AddSqlServerDevart(name!);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("name", Act);
+    }
+
+    [Test]
+    public void AddSqlServerDevart_WhenArgumentNameEmpty_ThrowArgumentException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var name = string.Empty;
+
+        // Act
+        void Act() => builder.AddSqlServerDevart(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>("name", Act);
+    }
+
+    [Test]
+    public void AddSqlServerDevart_WhenArgumentTagsNull_ThrowArgumentNullException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        var tags = default(string[]);
+
+        // Act
+        void Act() => builder.AddSqlServerDevart("Test", tags: tags);
+
+        // Assert
+        _ = Assert.Throws<ArgumentNullException>("tags", Act);
+    }
+
+    [Test]
+    public void AddSqlServerDevart_WhenArgumentNameIsAlreadyUsed_ThrowArgumentException()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder().Build();
+        var services = new ServiceCollection();
+        var builder = services.AddSingleton<IConfiguration>(configuration).AddHealthChecks();
+        const string? name = "Test";
+
+        // Act
+        void Act() => builder.AddSqlServerDevart(name).AddSqlServerDevart(name);
+
+        // Assert
+        _ = Assert.Throws<ArgumentException>(nameof(name), Act);
+    }
+}
