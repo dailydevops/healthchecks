bfbe7f526c84a1231c20d9ddbe262035b819cbf0
Martin Stühmer <me@samtrion.net>
2025-06-18 14:40:49 +0200

    chore: Simplified benchmarks (#829) [skip ci]

---
+++ benchmarks/Benchmarks.HealthChecks/Internals/BenchmarkServiceFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace Benchmarks.HealthChecks.Internals;
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+internal static class BenchmarkServiceFactory
+{
+    private static readonly IConfiguration _emptyConfiguration = new ConfigurationBuilder().Build();
+
+    public static BenchmarkHealthCheckService Create(Action<IHealthChecksBuilder> builder)
+    {
+        var serviceCollection = new ServiceCollection()
+            .AddSingleton(_emptyConfiguration)
+            .AddSingleton<BenchmarkHealthCheckService>();
+
+        var healthCheckBuilder = serviceCollection.AddHealthChecks();
+        builder.Invoke(healthCheckBuilder);
+
+        using (var serviceProvider = serviceCollection.BuildServiceProvider())
+        {
+            return serviceProvider.GetRequiredService<BenchmarkHealthCheckService>();
+        }
+    }
+}
