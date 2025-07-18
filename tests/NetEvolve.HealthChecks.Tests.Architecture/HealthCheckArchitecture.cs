9cbe67ace76e079d90d331d290e41b4caf65dacb
Martin Stühmer <me@samtrion.net>
2025-07-18 21:45:53 +0200

    feat: Added Implementation `NetEvolve.HealthChecks.SqlServer.Devart` (#817)
    * fix: Removed VersionOverride Ranges, because dependabot is broken (#848)
    
    * feat: Added Implementation `NetEvolve.HealthChecks.SqlServer.Devart`
    
    Closed: #815
    
    * fix(style): Reformatted code
    
    * fix: Added correct tags
    
    * chore: Updated snapshots
    
    * chore: Added license file
    
    * fix: Added `NetEvolve.HealthChecks.SqlServer.Devart` to solution
    
    * chore: Not necessary license file removed
    
    * fix: Restrict tests to `windows`
    
    * chore: Switched to centralized SharedType configuration
    
    * fix: Naming
    
    * chore: Added VersionOverride
    
    * fix(style): Reformatted code
    
    * fix: Removed VersionOverride Range

--- tests/NetEvolve.HealthChecks.Tests.Architecture/HealthCheckArchitecture.cs
+++ tests/NetEvolve.HealthChecks.Tests.Architecture/HealthCheckArchitecture.cs
@@ -56,6 +56,7 @@ internal static class HealthCheckArchitecture
             typeof(SQLite.SQLiteHealthCheck).Assembly,
             typeof(SQLite.Legacy.SQLiteLegacyHealthCheck).Assembly,
             typeof(SqlServer.SqlServerHealthCheck).Assembly,
+            typeof(SqlServer.Devart.SqlServerDevartHealthCheck).Assembly,
             typeof(SqlServer.Legacy.SqlServerLegacyHealthCheck).Assembly,
         ];
 
