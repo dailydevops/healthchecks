ebafac033511480e019257a12c114c8c02594243
Martin Stühmer <me@samtrion.net>
2025-10-05 23:46:08 +0200

    fix(test): Added missing architecture tests

--- tests/NetEvolve.HealthChecks.Tests.Architecture/HealthCheckArchitecture.cs
+++ tests/NetEvolve.HealthChecks.Tests.Architecture/HealthCheckArchitecture.cs
@@ -23,6 +23,7 @@ internal static class HealthCheckArchitecture
             typeof(Apache.ActiveMq.ActiveMqHealthCheck).Assembly,
             typeof(Apache.Kafka.KafkaHealthCheck).Assembly,
             // AWS
+            typeof(AWS.EC2.ElasticComputeCloudHealthCheck).Assembly,
             typeof(AWS.S3.SimpleStorageServiceHealthCheck).Assembly,
             typeof(AWS.SNS.SimpleNotificationServiceHealthCheck).Assembly,
             typeof(AWS.SQS.SimpleQueueServiceHealthCheck).Assembly,
