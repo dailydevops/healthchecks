85098e9995a15fb22dab8f35776b83eca4382f54
Martin Stühmer <me@samtrion.net>
2025-10-20 22:01:23 +0200

    fix: Added missing usings

--- tests/NetEvolve.HealthChecks.Tests.Unit/Apache.Kafka/KafkaHealthCheckTests.cs
+++ tests/NetEvolve.HealthChecks.Tests.Unit/Apache.Kafka/KafkaHealthCheckTests.cs
@@ -18,7 +18,7 @@ public sealed class KafkaHealthCheckTests
         // Arrange
         var optionsMonitor = Substitute.For<IOptionsMonitor<KafkaOptions>>();
         var serviceProvider = Substitute.For<IServiceProvider>();
-        var check = new KafkaHealthCheck(serviceProvider, optionsMonitor);
+        using var check = new KafkaHealthCheck(serviceProvider, optionsMonitor);
 
         // Act
         async Task Act() => _ = await check.CheckHealthAsync(null!, default);
@@ -33,7 +33,7 @@ public sealed class KafkaHealthCheckTests
         // Arrange
         var optionsMonitor = Substitute.For<IOptionsMonitor<KafkaOptions>>();
         var serviceProvider = Substitute.For<IServiceProvider>();
-        var check = new KafkaHealthCheck(serviceProvider, optionsMonitor);
+        using var check = new KafkaHealthCheck(serviceProvider, optionsMonitor);
         var context = new HealthCheckContext { Registration = new HealthCheckRegistration("Test", check, null, null) };
         var cancellationToken = new CancellationToken(true);
 
@@ -54,7 +54,7 @@ public sealed class KafkaHealthCheckTests
         // Arrange
         var optionsMonitor = Substitute.For<IOptionsMonitor<KafkaOptions>>();
         var serviceProvider = Substitute.For<IServiceProvider>();
-        var check = new KafkaHealthCheck(serviceProvider, optionsMonitor);
+        using var check = new KafkaHealthCheck(serviceProvider, optionsMonitor);
         var context = new HealthCheckContext { Registration = new HealthCheckRegistration("Test", check, null, null) };
 
         // Act
