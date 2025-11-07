fbd22b18291ec5449e8e7b0cffe0b82038388112
Alaa <180186936+Spacemonkay@users.noreply.github.com>
2025-11-07 22:34:24 +0100

    fix: Added IDisposable pattern to Redis HealthCheck (#998)
    Fixes #995

--- src/NetEvolve.HealthChecks.Redis/RedisHealthCheck.cs
+++ src/NetEvolve.HealthChecks.Redis/RedisHealthCheck.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,9 +11,10 @@ using SourceGenerator.Attributes;
 using StackExchange.Redis;
 
 [ConfigurableHealthCheck(typeof(RedisOptions))]
-internal sealed partial class RedisHealthCheck
+internal sealed partial class RedisHealthCheck : IDisposable
 {
     private ConcurrentDictionary<string, IConnectionMultiplexer>? _connections;
+    private bool _disposedValue;
 
     private async ValueTask<HealthCheckResult> ExecuteHealthCheckAsync(
         string name,
@@ -40,5 +42,29 @@ internal sealed partial class RedisHealthCheck
         _connections ??= new ConcurrentDictionary<string, IConnectionMultiplexer>(StringComparer.OrdinalIgnoreCase);
 
         return _connections.GetOrAdd(name, _ => ConnectionMultiplexer.Connect(options.ConnectionString!));
+    }
+
+    [SuppressMessage(
+        "Blocker Code Smell",
+        "S2953:Methods named \"Dispose\" should implement \"IDisposable.Dispose\"",
+        Justification = "As designed."
+    )]
+    private void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing && _connections is not null)
+            {
+                _ = Parallel.ForEach(_connections.Values, connection => connection.Dispose());
+                _connections.Clear();
+            }
+            _disposedValue = true;
+        }
+    }
+
+    void IDisposable.Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 }
