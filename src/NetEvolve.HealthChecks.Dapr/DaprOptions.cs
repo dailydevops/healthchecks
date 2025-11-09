98c90e15ecdd6e710920036eaa61c4266b9184fa
Hnogared <133124217+Hnogared@users.noreply.github.com>
2025-11-09 16:38:33 +0100

    chore(Dapr): Add keyed services support

--- src/NetEvolve.HealthChecks.Dapr/DaprOptions.cs
+++ src/NetEvolve.HealthChecks.Dapr/DaprOptions.cs
@@ -8,6 +8,15 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 public sealed record DaprOptions
 {
     /// <summary>
+    /// Gets or sets the key used to resolve the <c>DaprClient</c> from the service provider.
+    /// </summary>
+    /// <remarks>
+    /// When specified, the health check will resolve the <c>DaprClient</c> using <c>IServiceProvider.GetRequiredKeyedService</c>.
+    /// When null or empty, the health check will resolve the <c>DaprClient</c> using <c>IServiceProvider.GetRequiredService</c>.
+    /// </remarks>
+    public string? KeyedService { get; set; }
+
+    /// <summary>
     /// Timeout, which is granted when checking the Dapr sidecar. If the timeout is exceeded, the health check is classified as <see cref="HealthStatus.Degraded"/>.
     /// </summary>
     public int Timeout { get; set; } = 100;
