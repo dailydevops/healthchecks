1c7025836496667f19fb08ace3e0509b087dd84a
Martin Stühmer <me@samtrion.net>
2025-11-11 12:26:52 +0100

    test: Additional tests

--- tests/NetEvolve.HealthChecks.Tests.Integration/Ollama/OllamaHealthCheckTests.cs
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Ollama/OllamaHealthCheckTests.cs
@@ -1,11 +1,14 @@
 ﻿namespace NetEvolve.HealthChecks.Tests.Integration.Ollama;
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using NetEvolve.Extensions.TUnit;
 using NetEvolve.HealthChecks.Ollama;
+using OllamaSharp;
 
 [TestGroup(nameof(Ollama))]
 [ClassDataSource<OllamaContainer>]
@@ -111,5 +114,135 @@ public class OllamaHealthCheckTests : HealthCheckTestBase
                 };
                 _ = config.AddInMemoryCollection(values);
             }
+        );
+
+    [Test]
+    public async Task AddOllama_UseOptionsWithServiceProvider_Healthy() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddOllama(
+                    "TestContainerServiceProviderHealthy",
+                    options =>
+                    {
+                        options.Timeout = 10000;
+                        options.ClientMode = ClientMode.ServiceProvider;
+                    }
+                );
+            },
+            HealthStatus.Healthy,
+            serviceBuilder: services =>
+                services.AddSingleton<OllamaApiClient>(_ => new OllamaApiClient(new Uri(_container.BaseAddress)))
+        );
+
+    [Test]
+    public async Task AddOllama_UseOptionsWithKeyedService_Healthy()
+    {
+        const string serviceKey = "ollama-test-key";
+
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddOllama(
+                    "TestContainerKeyedServiceHealthy",
+                    options =>
+                    {
+                        options.KeyedService = serviceKey;
+                        options.Timeout = 10000;
+                        options.ClientMode = ClientMode.ServiceProvider;
+                    }
+                );
+            },
+            HealthStatus.Healthy,
+            serviceBuilder: services =>
+                services.AddKeyedSingleton(serviceKey, (_, _) => new OllamaApiClient(new Uri(_container.BaseAddress)))
+        );
+    }
+
+    [Test]
+    public async Task AddOllama_UseOptionsWithServiceProvider_Degraded() =>
+        await RunAndVerify(
+            healthChecks =>
+            {
+                _ = healthChecks.AddOllama(
+                    "TestContainerServiceProviderDegraded",
+                    options =>
+                    {
+                        options.Timeout = 0;
+                        options.ClientMode = ClientMode.ServiceProvider;
+                    }
+                );
+            },
+            HealthStatus.Degraded,
+            serviceBuilder: services =>
+                services.AddSingleton<OllamaApiClient>(_ => new OllamaApiClient(new Uri(_container.BaseAddress)))
+        );
+
+    [Test]
+    public async Task AddOllama_UseConfigurationWithServiceProvider_Healthy() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddOllama("TestContainerServiceProviderHealthy"),
+            HealthStatus.Healthy,
+            config =>
+            {
+                var values = new Dictionary<string, string?>
+                {
+                    { "HealthChecks:Ollama:TestContainerServiceProviderHealthy:Timeout", "10000" },
+                    {
+                        "HealthChecks:Ollama:TestContainerServiceProviderHealthy:ClientMode",
+                        nameof(ClientMode.ServiceProvider)
+                    },
+                };
+                _ = config.AddInMemoryCollection(values);
+            },
+            serviceBuilder: services =>
+                services.AddSingleton<OllamaApiClient>(_ => new OllamaApiClient(new Uri(_container.BaseAddress)))
+        );
+
+    [Test]
+    public async Task AddOllama_UseConfigurationWithKeyedService_Healthy()
+    {
+        const string serviceKey = "config-ollama-test-key";
+
+        await RunAndVerify(
+            healthChecks => healthChecks.AddOllama("TestContainerKeyedServiceHealthy"),
+            HealthStatus.Healthy,
+            config =>
+            {
+                var values = new Dictionary<string, string?>
+                {
+                    { "HealthChecks:Ollama:TestContainerKeyedServiceHealthy:KeyedService", serviceKey },
+                    { "HealthChecks:Ollama:TestContainerKeyedServiceHealthy:Timeout", "10000" },
+                    {
+                        "HealthChecks:Ollama:TestContainerKeyedServiceHealthy:ClientMode",
+                        nameof(ClientMode.ServiceProvider)
+                    },
+                };
+                _ = config.AddInMemoryCollection(values);
+            },
+            serviceBuilder: services =>
+                services.AddKeyedSingleton(serviceKey, (_, _) => new OllamaApiClient(new Uri(_container.BaseAddress)))
+        );
+    }
+
+    [Test]
+    public async Task AddOllama_UseConfigurationWithServiceProvider_Degraded() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddOllama("TestContainerServiceProviderDegraded"),
+            HealthStatus.Degraded,
+            config =>
+            {
+                var values = new Dictionary<string, string?>
+                {
+                    { "HealthChecks:Ollama:TestContainerServiceProviderDegraded:Timeout", "0" },
+                    {
+                        "HealthChecks:Ollama:TestContainerServiceProviderDegraded:ClientMode",
+                        nameof(ClientMode.ServiceProvider)
+                    },
+                };
+                _ = config.AddInMemoryCollection(values);
+            },
+            serviceBuilder: services =>
+                services.AddSingleton<OllamaApiClient>(_ => new OllamaApiClient(new Uri(_container.BaseAddress)))
         );
 }
