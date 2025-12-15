95f9e70673174aa7f7fb4064625a60d1f00abb90
Martin Stühmer <me@samtrion.net>
2025-12-15 07:18:14 +0100

    fix: Corrected parallel limiter (#1298)

--- tests/NetEvolve.HealthChecks.Tests.Integration/Internals/HealthCheckParallelLimit.cs
+++ tests/NetEvolve.HealthChecks.Tests.Integration/Internals/HealthCheckParallelLimit.cs
@@ -1,6 +1,16 @@
 ﻿namespace NetEvolve.HealthChecks.Tests.Integration.Internals;
 
+using System.Diagnostics;
+
 public record HealthCheckParallelLimit : IParallelLimit
 {
-    public int Limit => Math.Clamp(Environment.ProcessorCount, Math.Min(4, Environment.ProcessorCount), 12);
+    public int Limit
+    {
+        get
+        {
+            var limit = Math.Clamp(Environment.ProcessorCount - 1, Math.Min(4, Environment.ProcessorCount - 1), 16);
+            Debug.WriteLine($"Parallel Limit: {limit}");
+            return limit;
+        }
+    }
 }
