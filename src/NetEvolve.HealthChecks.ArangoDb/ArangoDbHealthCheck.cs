3a974f3456d457b2487ba36fd891f3ed6f0d5c85
Martin Stühmer <me@samtrion.net>
2025-12-15 23:07:58 +0100

    chore: Simplified ArangoDB

--- src/NetEvolve.HealthChecks.ArangoDb/ArangoDbHealthCheck.cs
+++ src/NetEvolve.HealthChecks.ArangoDb/ArangoDbHealthCheck.cs
@@ -1,9 +1,11 @@
 ﻿namespace NetEvolve.HealthChecks.ArangoDb;
 
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading.Tasks;
 using ArangoDBNetStandard;
 using ArangoDBNetStandard.CursorApi.Models;
+using ArangoDBNetStandard.Transport.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using NetEvolve.Extensions.Tasks;
@@ -12,6 +14,8 @@ using SourceGenerator.Attributes;
 [ConfigurableHealthCheck(typeof(ArangoDbOptions))]
 internal sealed partial class ArangoDbHealthCheck
 {
+    private ConcurrentDictionary<string, ArangoDBClient>? _arangoDbClients;
+
     private async ValueTask<HealthCheckResult> ExecuteHealthCheckAsync(
         string name,
 #pragma warning disable S1172 // Unused method parameters should be removed
@@ -21,8 +25,7 @@ internal sealed partial class ArangoDbHealthCheck
         CancellationToken cancellationToken
     )
     {
-        var clientProvider = _serviceProvider.GetRequiredService<ArangoDbClientProvider>();
-        var client = clientProvider.GetClient(name, options, _serviceProvider);
+        var client = GetClient(name, options, _serviceProvider);
 
         var commandTask = options.CommandAsync.Invoke(client, cancellationToken);
 
@@ -40,5 +43,31 @@ internal sealed partial class ArangoDbHealthCheck
             .ConfigureAwait(false);
 
         return cursor?.Result?.Any() == true;
+    }
+
+    private ArangoDBClient GetClient(string name, ArangoDbOptions options, IServiceProvider serviceProvider)
+    {
+        if (options.Mode == ArangoDbClientCreationMode.ServiceProvider)
+        {
+            return string.IsNullOrWhiteSpace(options.KeyedService)
+                ? serviceProvider.GetRequiredService<ArangoDBClient>()
+                : serviceProvider.GetRequiredKeyedService<ArangoDBClient>(options.KeyedService);
+        }
+
+        _arangoDbClients ??= new ConcurrentDictionary<string, ArangoDBClient>(StringComparer.OrdinalIgnoreCase);
+
+        return _arangoDbClients.GetOrAdd(name, _ => CreateClient(options));
+    }
+
+    private static ArangoDBClient CreateClient(ArangoDbOptions options)
+    {
+        var addressUri = new Uri(options.TransportAddress!);
+
+        var transport =
+            !string.IsNullOrWhiteSpace(options.Password) && !string.IsNullOrWhiteSpace(options.Username)
+                ? HttpApiTransport.UsingBasicAuth(addressUri, options.Username, options.Password)
+                : HttpApiTransport.UsingNoAuth(addressUri);
+
+        return new ArangoDBClient(transport);
     }
 }
