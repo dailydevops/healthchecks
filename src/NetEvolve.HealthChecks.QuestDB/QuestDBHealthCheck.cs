e3fa531b32b0d9e98114b502b52c448e77812a6c
Alaa <180186936+Spacemonkay@users.noreply.github.com>
2026-03-03 16:00:14 +0100

    feat: Added `NetEvolve.HealthChecks.QuestDB` (#1476)
    * feat: Added `NetEvolve.HealthChecks.QuestDB`
    
    * style: Reformatted whole solution
    
    * test(PublicAPI): Added Public API Definition<
    
    * fix(test): Updated tests
    
    * fix(test): Added missing Verified files
    
    * fix(test): Corrected class name from QuestDBHealthCheckTests to QdrantHealthCheckTests
    
    * fix(editorconfig): Remove unnecessary diagnostic severity settings
    
    * fix: Update QuestDB health check implementation and documentation
    
    ---------
    
    Co-authored-by: Martin Stühmer <me@samtrion.net>

---
+++ src/NetEvolve.HealthChecks.QuestDB/QuestDBHealthCheck.cs
@@ -0,0 +1,42 @@
+﻿namespace NetEvolve.HealthChecks.QuestDB;
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetEvolve.Extensions.Tasks;
+using SourceGenerator.Attributes;
+
+[ConfigurableHealthCheck(typeof(QuestDBOptions))]
+internal sealed partial class QuestDBHealthCheck
+{
+    private async ValueTask<HealthCheckResult> ExecuteHealthCheckAsync(
+        string name,
+        HealthStatus failureStatus,
+        QuestDBOptions options,
+        CancellationToken cancellationToken
+    )
+    {
+        var httpClientFactory = _serviceProvider.GetRequiredService<IHttpClientFactory>();
+
+        var httpClient = httpClientFactory.CreateClient(name);
+        using var request = new HttpRequestMessage(HttpMethod.Get, options.StatusUri);
+
+        var (isTimelyResponse, response) = await httpClient
+            .SendAsync(request, cancellationToken)
+            .WithTimeoutAsync(options.Timeout, cancellationToken)
+            .ConfigureAwait(false);
+
+        using (response)
+        {
+            return response.IsSuccessStatusCode
+                ? HealthCheckState(isTimelyResponse, name)
+                : HealthCheckUnhealthy(
+                    failureStatus,
+                    name,
+                    $"Unexpected status code {response.StatusCode}. Expected: between 200 and 300"
+                );
+        }
+    }
+}
---
+++ tests/NetEvolve.HealthChecks.Tests.Integration/QuestDB/QuestDBHealthCheckTests.cs
@@ -0,0 +1,97 @@
+﻿namespace NetEvolve.HealthChecks.Tests.Integration.QuestDB;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetEvolve.Extensions.TUnit;
+using NetEvolve.HealthChecks.QuestDB;
+using NetEvolve.HealthChecks.Tests.Integration.QuestDB.Container;
+
+[TestGroup(nameof(QuestDB))]
+[TestGroup("Z03TestGroup")]
+[ClassDataSource<QuestDBDatabase>(Shared = SharedType.PerClass)]
+public sealed class QuestDBHealthCheckTests : HealthCheckTestBase
+{
+    private readonly QuestDBDatabase _database;
+
+    public QuestDBHealthCheckTests(QuestDBDatabase database) => _database = database;
+
+    [Test]
+    public async Task AddQuestDB_UseOptions_Healthy() =>
+        await RunAndVerify(
+            healthChecks =>
+                healthChecks.AddQuestDB(
+                    "TestContainerHealthy",
+                    options =>
+                    {
+                        options.Timeout = 10000;
+                        options.StatusUri = _database.StatusUri.ToString();
+                    }
+                ),
+            HealthStatus.Healthy
+        );
+
+    [Test]
+    public async Task AddQuestDB_UseOptions_Degraded() =>
+        await RunAndVerify(
+            healthChecks =>
+                healthChecks.AddQuestDB(
+                    "TestContainerDegraded",
+                    options =>
+                    {
+                        options.Timeout = 0;
+                        options.StatusUri = _database.StatusUri.ToString();
+                    }
+                ),
+            HealthStatus.Degraded
+        );
+
+    [Test]
+    public async Task AddQuestDB_UseOptions_Unhealthy() =>
+        await RunAndVerify(
+            healthChecks =>
+                healthChecks.AddQuestDB(
+                    "TestContainerUnhealthy",
+                    options =>
+                    {
+                        options.Timeout = 0;
+                        options.StatusUri = "invalid";
+                    }
+                ),
+            HealthStatus.Unhealthy
+        );
+
+    [Test]
+    public async Task AddQuestDB_UseConfiguration_Healthy() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddQuestDB("TestContainerConfigHealthy"),
+            HealthStatus.Healthy,
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:QuestDB:TestContainerConfigHealthy:Timeout", "10000" },
+                    { "HealthChecks:QuestDB:TestContainerConfigHealthy:StatusUri", _database.StatusUri.ToString() },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+
+    [Test]
+    public async Task AddQuestDB_UseConfiguration_Degraded() =>
+        await RunAndVerify(
+            healthChecks => healthChecks.AddQuestDB("TestContainerConfigDegraded"),
+            HealthStatus.Degraded,
+            config =>
+            {
+                var values = new Dictionary<string, string?>(StringComparer.Ordinal)
+                {
+                    { "HealthChecks:QuestDB:TestContainerConfigDegraded:Timeout", "0" },
+                    { "HealthChecks:QuestDB:TestContainerConfigDegraded:StatusUri", _database.StatusUri.ToString() },
+                };
+                _ = config.AddInMemoryCollection(values);
+            }
+        );
+}
